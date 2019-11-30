071f9f78ae7c9c168944d901d71318b320547f4b
jki14 <jki14wz@gmail.com>
2019-11-30 23:03:05 +0900

    Multiple Updates
    1. Update WoW character name in symbolic scripts
    2. Add the network logs directory into the gitignore

--- wow/symbolic.cmd
+++ wow/symbolic.cmd
@@ -9,5 +9,5 @@ MKLINK /J WTF\Account\83215426#1\Tichondrius\Morgianu WTF\Account\83215426#1\Tic
 MKLINK /J WTF\Account\83215426#1\凤凰之神\依然活著 WTF\Account\83215426#1\Tichondrius\Masoshonen
 MKLINK /J WTF\Account\83215426#1\凤凰之神\依然抹布洗 WTF\Account\83215426#1\Tichondrius\Masoshonen
 MKLINK /J WTF\Account\83215426#1\凤凰之神\仍然活著 WTF\Account\83215426#1\Tichondrius\Masoshonen
-MKLINK /J WTF\Account\83215426#1\熊猫酒仙\Positionzero WTF\Account\83215426#1\Tichondrius\Masoshonen
+MKLINK /J WTF\Account\83215426#1\熊猫酒仙\魔装少年 WTF\Account\83215426#1\Tichondrius\Masoshonen
 PAUSE
--- wow/symbolic.py
+++ wow/symbolic.py
@@ -25,7 +25,7 @@ def characters():
   bar = [
     'Tichondrius - Morgiane', 'Tichondrius - Morgianu',
     '凤凰之神 - 依然活著', '凤凰之神 - 依然抹布洗', '凤凰之神 - 仍然活著',
-    '熊猫酒仙 - Positionzero'
+    '熊猫酒仙 - 魔装少年'
   ]
   for path in [os.path.join(prefix, *c.split(' - ')) for c in bar]:
     os.symlink(foo, path)
