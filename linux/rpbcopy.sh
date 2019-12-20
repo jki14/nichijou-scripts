862c8595f409b9a7e7799e5f06325157486b789a
jki14 <jki14wz@gmail.com>
2019-12-20 23:02:13 +0900

    Update remote pbcopy solution
    1. rpbopy is now workable with WSL

--- linux/rpbcopy.sh
+++ linux/rpbcopy.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 # ssh-config: RemoteForward 3084 127.0.0.1:3084
-# remote-cmd: nc -q0 127.0.0.1 3084
-if [ -f /proc/version ]; then
-    # Linux & WSL
-    # while (true); do nc -l -p 3084 | pbcopy; done &
-    # TODO
+# remote-cmd: echo 'Hello World' | nc -q0 127.0.0.1 3084
+
+PIDFILE=~/var/run/rpbcopy.pid
+LOGFILE=~/var/log/rpbcopy.log
+
+if test -f /proc/version && grep -q Microsoft /proc/version; then
+    # WSL
+    PBCOPY=/mnt/c/WINDOWS/system32/clip.exe
+    COMMAND="while (true); do nc -l -p 3084 | $PBCOPY; done"
+elif test -f /proc/version; then
+    # Linux
+    COMMAND="TODO"
 else
-    while (true); do nc -l 3084 | pbcopy; done &
+    # OSX
+    PBCOPY=/usr/bin/pbcopy
+    COMMAND="while (true); do nc -l 3084 | $PBCOPY; done"
 fi
+
+eval "$COMMAND &"
