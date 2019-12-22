ec882c098d4b5f38e155c3d44189054a684c3a89
jki14 <jki14wz@gmail.com>
2019-12-22 16:58:59 +0900

    Disable multiple pbcopy forward
    1. Disable rpbcopy in ssh-sessions since it currently works incorrectly
    2. Use both 3084/3085 ports to prevent conflicts

--- linux/rpbcopy.sh
+++ linux/rpbcopy.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
-# ssh-config: RemoteForward 3084 127.0.0.1:3084
-# remote-cmd: echo 'Hello World' | nc -q0 127.0.0.1 3084
+# ssh-config: RemoteForward 3085 127.0.0.1:3084
+# remote-cmd: echo 'Hello World' | nc -q0 127.0.0.1 3085
 
 if test -n "$SSH_TTY"; then
     # Remote Session
-    if test -f /proc/version && grep -q Ubuntu /proc/version; then
-        PBCOPY='nc -q0 127.0.0.1 3084'
-    else
-        PBCOPY='nc -w0 127.0.0.1 3084'
-    fi
+    # TODO
+    # if test -f /proc/version && grep -q Ubuntu /proc/version; then
+    #     PBCOPY='nc -q0 127.0.0.1 3085'
+    # else
+    #     PBCOPY='nc -w0 127.0.0.1 3085'
+    # fi
 elif test -f /proc/version && grep -q Microsoft /proc/version; then
     # WSL
     PBCOPY=/mnt/c/WINDOWS/system32/clip.exe
