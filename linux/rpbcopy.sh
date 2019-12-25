ffbdd0d8eff009a563873a8f1adccd4220e3904f
jki14 <jki14wz@gmail.com>
2019-12-25 12:58:34 +0900

    Update local pbcopy and remote pbcopy
    1. Add rstrip feature into local pbcopy and remote pbcopy

--- linux/rpbcopy.sh
+++ linux/rpbcopy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # ssh-config: RemoteForward 3085 127.0.0.1:3084
-# remote-cmd: echo 'Hello World' | nc -q0 127.0.0.1 3085
+# remote-cmd: echo 'Hello World' | nc 127.0.0.1 3085
 
 if test -n "$SSH_TTY"; then
     # Remote Session
@@ -10,6 +10,7 @@ if test -n "$SSH_TTY"; then
     # else
     #     PBCOPY='nc -w0 127.0.0.1 3085'
     # fi
+    :
 elif test -f /proc/version && grep -q Microsoft /proc/version; then
     # WSL
     PBCOPY=/mnt/c/WINDOWS/system32/clip.exe
@@ -21,12 +22,14 @@ else
     PBCOPY=/usr/bin/pbcopy
 fi
 
+RSTRIP='python3 -c "import sys; sys.stdout.write(sys.stdin.read().rstrip())"'
+
 if test -f /proc/version && grep -q Ubuntu /proc/version; then
     # Mint/Ubuntu/WSL
-    RPBCOPY="while (true); do nc -l -p 3084 | $PBCOPY; done"
+    RPBCOPY="while (true); do nc -l -p 3084 | $RSTRIP | $PBCOPY; done"
 else
     # OSX
-    RPBCOPY="while (true); do nc -l 3084 | $PBCOPY; done"
+    RPBCOPY="while (true); do nc -l 3084 | $RSTRIP | $PBCOPY; done"
 fi
 
 eval "$RPBCOPY &"
