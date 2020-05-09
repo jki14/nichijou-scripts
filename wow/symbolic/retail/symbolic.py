e34a8fa035a5be3f1f8161e424068f6700371355
jki14 <jki14wz@gmail.com>
2020-05-09 23:41:25 +0900

    Update WoW Retail Symbolic
    1. Add Stormrage - Masoshonen

--- wow/symbolic/retail/symbolic.py
+++ wow/symbolic/retail/symbolic.py
@@ -26,6 +26,7 @@ def characters():
   foo = os.path.join(prefix, 'Tichondrius', 'Masoshonen')
   bar = [
     'Tichondrius - Morgiane', 'Tichondrius - Morgianu',
+    'Stormrage - Masoshonen',
     '熊猫酒仙 - 魔装少年', '凤凰之神 - 依然活著', '凤凰之神 - 仍然活著',
   ]
   for path in [os.path.join(prefix, *c.split(' - ')) for c in bar]:
