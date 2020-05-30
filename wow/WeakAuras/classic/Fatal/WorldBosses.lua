0dd1b6ef611509351bf5cd0e19181135ff159a3b
jki14 <jki14wz@gmail.com>
2020-05-30 14:02:15 +0900

    Update WoW Classic WeakAura World Bosses
    Improve guild message sending.

--- wow/WeakAuras/classic/Fatal/WorldBosses.lua
+++ wow/WeakAuras/classic/Fatal/WorldBosses.lua
@@ -1,5 +1,5 @@
 --release
-!TwvxUnUnq47Ycu4eGMaz5)lqFWjXoB2M1jW0zdw0uztlnsIi0KQKujBCr1jOOf9HTNJ(ypqTyVgDiLKLrsECHnSjNz4WVz43mtq7GfbKnbKi7xC5NUkowdMGPh1Fy)o((ED88861TN)WG4J65hqEQYGzEbeQimvQUwYeMaYPtMTyY8asOuYJKpkipYYGGtAemjkbcwhqyHsHvUrXssaLgV2zoyujWUKMROc6gOsj5KCLGjskwnosbckNjGGziAUK9Gt8jYnRdqquFgCzeSopoEXtiiiVD883p9Mldi6u5JNYLc0iejgNYWCTrUP()LLcHhaBqLly4VzC6tiUWDvIP5g5oBElq5MuuRgMHxVn0WLlDhfxRZx7mKGOH9PaYYthtwSKSy8Cmo1zaNFrufKRn9Af4mLC9KlrqBJdcc8ReLbWvIXHg2d1OuhqU(YXFCY8LOppFYILN(2XZoFYznr2rhvSQk5w8U4CbEAP4GdlExXQYpCziLxSkwklw99fR(LIv)AJsu6p2QFBVrT(jN2wJ3MRGePU1ZSPTFNrdQn6sPkQy1pq3ULE)lmS7WHd3ziysLIxZKDx4KnGIP1VInJ8QTzbfuu1R5MDq6JAPaPp7zJnXub9UJ2HOZuaDJ2q53d77WQK0AQYzMg4qO5G(FlU0O0zCM5G3C0BWT3GV8NFZfNDqldvLaMwhE4E5AwC5DI(bVptkiA0v)rbgKWBF0YHgTGiQztTjXuUgTb1TJbNYIAO1os8Evw1CcuHLi7islkv)EjEUzh12ZQbRPbfwP9bScfzly12OaY9aKng5SHM5uKdzRHOc2g7AH15ystzCLYUIibwOHfIyTS1GQklnG1)iJNaunG18GibREM1PuWIMdIGydLj(k6UyMGPt)65qxxgn1yDGR1wMsMOaTglGdzLPnpxz9Cib310QBnwDaQLA2wmJ3D3EztVvd8jZYqjxQk7nuzrTe0n2V(bthoAq7Hd6pYR7aF)UEJS9P7AXl6f01OvnN(beuR5UwuvsaBtzc5NZPkOy10CoYWVnLHHtvocbFPPLXBkWssXXdJ88g0Eu)Ed86mSF3o(4T2DqaHlPUXiytzLmpRe62UH2oNkBkYUje7wtn2DHCQw7gbLZnmN5gxxXWxk0MREMquSYLzlFwCtwiBLi)2McFrUY(Bh3V15f7tnZu90yAMvTaZ(NX0umzf5y52g(2jgYWhQliA3UJh(0XWcVBLkE0jsTgNRGeiE8ZMjgRWbdixIAOUXzLdoNALwICYPZNmzgMDSE78SnBN(u)BYJ(GdHXSKs0Xe2lhQae6HRY2d7pYIqI60UiDWBqp)(ThGmcpp7ltFKOY02Xyxe6GENUT71d9cplTcrH1EQPiosUKlLzw2covvttWxUQUhpFisD3Ow)3F8NF53)7)9F(RV85FZMq(ouwXQJpEx)Y2vniTJk3RbzvhmTmxeTubzaYpM1ZHHQ7Epyeq(MqNkN5UHSUfKlSnTIPHWDJJIWW5UBb69JXYt9DxlFeuULVhIy07i2tOVtNYY0pMY0goCSmjX52QOCDtCxwlGn1V8SYhcwDgANT73FPIBIKb)JBp4ymoEaZYD9TCUG)p
+!TE16UTTrw43L8hjVyTbPURfy)HSTKVuBzdr5eKwVuCK4iQbMIJ6mK2XUOe1yrARt26GG2K02GUytwGfOiajojfWPxwNxMizL3IEMHxeLtAq)rGmOhoNRZzoFNZH6Q6n116RRzk(dw2HAtzWI6QY)YiFMv(mN4PRU2(B0Tlh7Qxxrxd50PhLTjL4aewOA9MvBi0b12KUNJ2EKby95NSrvtlSEBqQoUeQdxypUlI5kwysBztPdeS3hZ5ilCRoECxk4Bt2XDFqHAARxPrZgvwzrPuH0e6nAjirKSD9CKgZ34kuMT58uohZxNBLEg)v9n8nSPDq2(gDPuFJ)UVXN4B8PabjjyVpkvLd8yylkp1)qsp14Jo78F6HV((No(23F0DFIVr6XF9p86Z(UzsLqO1OmtFJpaDWbODIeC4x9GrhD8WRFZPye72J6eR7JFkO7rh(JGwp)xFjO1)MVXOV)Ed))pDk1xTpMr4jDPXh)OXhF0REXpKuYXF7ThFRVykjBIWmelsWrp)Kr38OHN8njLA4x(5J)LhnLuxLtDmz4iXE1VDJr)3hp8StHOWejfV)SdbAtj6ImmQpChBVdo2Sdp9bJE4)B8NDDrW7K)54NCNx9Yhm6WNmLGRImX9jmFJnyOo2X2E4n(pGHo)Pph8AWQJU3TEBkjH9947ShLAgFK)(hp(ONbU)O)99MkW0d7BSmKfJz2ihZjX2ZU)5h9ZdV(jjzUgeeTrt45wpdcJqYqsEQW7HD2fnXZp)WtgFYPXExyExBetsMJTXDCtR(x9n2YH4wh1hNofanSWUPMjmvnIxH(H1GEPSGDNwNdOCjFlHD)qQdUj(AUrz7rKeQawhQcXUtRI(CRW49TU95hFYREXDp)o)lb2bsqcpLrSi))CZf5iVnkP8nio)XcMW8AyhZf6HCxpaiNg4bIiPwARvwBXuZ8EKNWlk4WtKoJiWRMZ3WKgqs8BHwnjasBUkDHSI0zv8n(la7aJr1uIIQr)(Z4zIFaBjEBads7s7s5UWcRy7mtySdUWmX2O9XMHHSoaKYfhPpqx(RkF4V6fRWjlpYPEoMIQQYfTy4bG461ZhUHU2kIK(UOo4TRyAcvL3(ky0ov8yi(2Bs3dZKlxhBsqBRjKGVnVhzaFVEeURnEoQLL0oHfCNx0HOlXHW7bv1H1WYO67HS0g2giahxllmJh0SPz8gILiWMoagiKO28Emhi44BubQd5GSjoyrVjT1i7k3EEAFHw18egtBGiCXatIB71TBtzpJLR0y9ABTgCQ7r3BbBaxWfDmcAOe1SWZbVlw0jd55sJ2UvpIzsE4yb6uerdzEzmY2Txm7bQmKg3RTCLg4iKRPR1AHkAnBP1eAG9M9OE39KIQSuqvPCurL4UtjORMjB5IrmmvNOKmLRuPsXmf0f6cKJnsu7MPPxwjIEytLliESle19iG(SZg7M5kh7bt1M4cmMpRASUUyxH3vT0ca2dWu8b2e30xA2lfwAfqJlMO0AyiN0nzDvxO29eakd7cPFWUmpCcqxssDr2CSehcx5dW22RyYds)L3(CDTnxRYvR2OfCXVu1MTwy5k1xQkm)susEIuLnzyzQI2MvxdYxfPWAqo7gob5UB4ubsw2flZeBjt4Npi1pb(jEYjjsdjfOza51Pq2C9zvvcaNa2hWtxgWHqciGPkRRTdgpOcCk642abPLYb2Ci9fRDMEKnaLk2mmLNJ7a308iqLdGXGZpIJbKn2XcGiWGKYnAoHbWj6JioVhvxCXN3tk0vuiHJCfktoL5ag1IbL4HlQoKGWMI86Rb2kCS2GcATbWhM1IozC5WD4KdWI5P1CHEZTIg52nM(BmeETsLlQwQyHYk5kMjtoLY6DNnFo5G5ktgmpu6DbNQTTScv4oyXC3AAFShIbJ5uZZgqlxPhbooHXcW5dyn482dtS65QxRSIsr1YfYxujBPc5YwaSAUI6A2uK8BfGsVmQ3GaxxKnkkFYeHOoW4rYX777z7sKmeYshOsn0)PTeN05nyrgzUWMW2mzKn4AHa3IqgP9GEizVb57I(By7Ux4drmjCrVGveCuxnBo185N8XiI5IwKWrqOYuMJlQ5lAjq7SBeCqvnRcCnrayDIMR6Axl8tGQnBHsfYMjJswffL85YRwuCXanM6YaGnKxHCdCY9iMq(vTCWTOsX8zkOwewwUKiGwq4soDjwHXrHXwAq)dQTFHT8mVSiml8hCOpcFU1gdI(8jxPSMKeVh8fz1e2piEPTqJQvRRRDafQiuxc7HtqM5ulohen2fCVCzePp6)o
 
 -- trigger
 function()
@@ -11,6 +11,7 @@ function()
   foo['14890'] = 'Taerar'
   foo['14887'] = 'Ysondre'
   -- foo['12498'] = 'Dreamstalker'
+  -- foo['5317'] = 'Jademir Oracle'
   local bar = select(6, strsplit("-", UnitGUID('target')))
   if foo[bar] then
     return true
@@ -19,9 +20,47 @@ function()
 end
 
 -- message
-function()
-  return '发现世界Boss: ' .. select(1, UnitName('target'))
+function WorldBossesMsg()
+  local foo = { }
+
+  foo['Azuregos'] = '艾索雷葛斯 (蓝龙)'
+  foo['Lord Kazzak'] = '卡扎克'
+  foo['Lethon'] = '莱索恩 (绿龙: 暗影)'
+  foo['Emeriss'] = '艾莫莉丝 (绿龙: 蘑菇)'
+  foo['Taerar'] = '泰拉尔 (绿龙: 分身)'
+  foo['Ysondre'] = '伊森德雷 (绿龙:德鲁伊)'
+  foo['Dreamstalker'] = '巡梦者 (调试信息)'
+  foo['Jademir Oracle'] = '加德米尔龙族 (调试信息)'
+
+  foo['Duskwood'] = '暮色森林'
+  foo['The Hinterlands'] = '辛特兰'
+  foo['Feralas'] = '菲拉斯'
+  foo['Ashenvale'] = '灰谷'
+
+  local bar = select(1, UnitName('target'))
+  bar = foo[bar] or bar
+
+  local pos = GetZoneText()
+  pos = foo[pos] or pos
+
+  local msg = '发现世界Boss: '
+  msg = msg .. bar
+  msg = msg .. ' in '
+  msg = msg .. pos
+
+  SendChatMessage(msg, 'GUILD')
+  SendChatMessage(msg, 'GUILD')
+  SendChatMessage(msg, 'GUILD')
+
+  for i = 1, 14 do
+    C_Timer.After(30 * i, function()
+      SendChatMessage(msg, 'GUILD')
+    end)
+    print(tostring(30 * i) .. 's delayed msg create')
+  end
 end
+
+WorldBossesMsg()
 
 -- macro
 /tar Azuregos
