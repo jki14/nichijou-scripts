bba3c43ac2d9804090f4453635453fc3832ae7d6
jki14 <jki14wz@gmail.com>
2020-06-26 19:59:50 +0900

    Update WoW Classic WeakAuras CurrentStats
    Show raw current armor.

--- wow/WeakAuras/classic/CurrentStats/custom.lua
+++ wow/WeakAuras/classic/CurrentStats/custom.lua
@@ -1,3 +1,5 @@
 function()
-    return 'Current Speed: ' .. string.format('%.0f', select(1, GetUnitSpeed('player')) / 7.0 * 100.0)
+    foo = 'Armor: ' .. tostring(select(2, UnitResistance('player', 0)))
+    foo = foo .. '\n' .. 'Speed: ' .. string.format('%.0f', select(1, GetUnitSpeed('player')) / 7.0 * 100.0)
+    return foo
 end
