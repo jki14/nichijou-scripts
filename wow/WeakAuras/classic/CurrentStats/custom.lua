07aab42e8ff215956867dee2d272477b0afcb924
jki14 <jki14wz@gmail.com>
2020-06-27 20:08:11 +0900

    Update WoW Classic WeakAuras CurrentStats
    Show physical damage reduction based on current armor (fight with 63).

--- wow/WeakAuras/classic/CurrentStats/custom.lua
+++ wow/WeakAuras/classic/CurrentStats/custom.lua
@@ -1,5 +1,6 @@
 function()
-    foo = 'Armor: ' .. tostring(select(2, UnitResistance('player', 0)))
+    local armor = select(2, UnitResistance('player', 0))
+    local foo = 'Armor: ' .. tostring(armor) .. ' (' .. string.format('%.2f', min(armor * 100.0 / (armor + 5755), 75.0)) .. '%)'
     foo = foo .. '\n' .. 'Speed: ' .. string.format('%.0f', select(1, GetUnitSpeed('player')) / 7.0 * 100.0)
     return foo
 end
