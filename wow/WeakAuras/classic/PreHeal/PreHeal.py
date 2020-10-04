eb020f6ed5405728c4dc0cd9371a2092ee0921a3
jki14 <jki14wz@gmail.com>
2020-10-04 20:16:29 +0900

    Add python PreHeal for WoW Classic WeakAuras
    1. Implement the python PreHeal with keyboard and pyautogui.

---
+++ wow/WeakAuras/classic/PreHeal/PreHeal.py
@@ -0,0 +1,54 @@
+import sys
+
+from datetime import datetime
+from keyboard import wait
+from pyautogui import pixel, press
+from time import sleep
+
+
+def distance2(lhs, rhs):
+    foo = 0
+    m = len(lhs)
+    for i in range(m):
+        foo += (lhs[i] - rhs[i]) ** 2
+    return foo
+
+
+def run():
+    cords = [
+        (0, 0, 0),          # 0
+        (255, 124, 10)      # 1
+    ]
+
+    wait('j')
+    point = pixel(16, 64)
+    eps, idx = min([(distance2(point, cords[i]), i) for i in range(len(cords))])
+    if idx != 0:
+        press('k')
+    sleep(0.010)
+    press('e')
+    sys.stdout.write('[%s] PreHeal index %d with %d epsilon.\n' % (
+                     datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                     idx, eps))
+    sleep(0.010)
+
+
+def guard():
+    while True:
+        try:
+            run()
+            break
+        except OSError:
+            sys.stdout.write('[%s] OSError.\n' %
+                             datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            sleep(0.010)
+            pass
+
+
+def main():
+    while True:
+        guard()
+
+
+if __name__ == '__main__':
+    main()
