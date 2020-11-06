71e54fd6947dd2a0c5da17c82b11559819463646
jki14 <jki14wz@gmail.com>
2020-11-06 23:18:08 +0900

    Update WoW Classic WeakAruas EnergyTick
    Ignore the Mana regeneration less or equal than 36.

--- wow/WeakAuras/classic/EnergyTick/EnergyTick.lua
+++ wow/WeakAuras/classic/EnergyTick/EnergyTick.lua
@@ -19,7 +19,7 @@ function(a, event, unit, power)
   elseif unit == 'player' then
     if power == 'MANA' and
         11 == select(2, UnitClassBase('player')) and
-        currMana > (aura_env.lastMana or 0) then
+        currMana > (aura_env.lastMana or 0) + 36 then
       flag = true
     elseif power == 'ENERGY' and currEnergy > (aura_env.lastEnergy or 0) then
       flag = true
