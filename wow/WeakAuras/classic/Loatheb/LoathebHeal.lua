16cda7d7c7f83c9848bbaf3ad8d25ad3b6761bd6
jki14 <jki14wz@gmail.com>
2020-12-11 21:40:49 +0900

    Update WoW Classic WeakAruas LoathebHeal
    1. Cache the HealerList on Init.
    2. Make the current and the next healer format consistent.

--- wow/WeakAuras/classic/Loatheb/LoathebHeal.lua
+++ wow/WeakAuras/classic/Loatheb/LoathebHeal.lua
@@ -1,3 +1,22 @@
+-- on-init
+local healerList = { }
+healerList['霪雨滑人'] = 1
+healerList['丑尼娜萨德星'] = 2
+healerList['犇犇萌牛丶'] = 3
+healerList['小琪琪'] = 4
+healerList['假女乃亮'] = 5
+healerList['尐酒窝'] = 6
+healerList['你的小野兽'] = 7
+healerList['上古巨神'] = 8
+healerList['一株小盆栽'] = 9
+healerList['皮皮卡'] = 10
+healerList['Pioa'] = 11
+healerList['依然活着'] = 12
+
+wa_global = wa_global or { }
+wa_global.loatheb = wa_global.loatheb or { }
+wa_global.loatheb.healerList = healerList
+
 -- trigger: COMBAT_LOG_EVENT_UNFILTERED,PLAYER_REGEN_DISABLED
 function(e, timestamp, event, hideCaster, sourceGUID, sourceName,
          sourceFlags, sourceRaidFlags, destGUID, destName, destFlags,
@@ -19,19 +38,9 @@ function(e, timestamp, event, hideCaster, sourceGUID, sourceName,
         return false
     end
 
-    local healerList = { }
-    healerList['霪雨滑人'] = 1
-    healerList['丑尼娜萨德星'] = 2
-    healerList['犇犇萌牛丶'] = 3
-    healerList['小琪琪'] = 4
-    healerList['假女乃亮'] = 5
-    healerList['尐酒窝'] = 6
-    healerList['依然活着'] = 7
-    healerList['上古巨神'] = 8
-    healerList['一株小盆栽'] = 9
-    healerList['皮皮卡'] = 10
-    healerList['Pioa'] = 11
-    healerList['你的小野兽'] = 12
+    local healerList = wa_global and
+                       wa_global.loatheb and
+                       wa_global.loatheb.healerList or { }
 
     local myname = destName
     if healerList[myname] then
@@ -40,10 +49,10 @@ function(e, timestamp, event, hideCaster, sourceGUID, sourceName,
         local nxt = ''
         for name, offset in pairs(healerList) do
             if offset == 1 then
-                nxt = tostring(offset) .. '-' .. name
+                nxt = tostring(offset) .. ' - ' .. name
             end
             if offset == idx + 1 then
-                nxt = tostring(offset) .. '-' .. name
+                nxt = tostring(offset) .. ' - ' .. name
                 break
             end
         end
