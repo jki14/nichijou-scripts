4687e2eda2b8f4132e85b0b0d390def41b73c256
jki14 <jki14wz@gmail.com>
2020-12-15 01:41:09 +0900

    Hotfix WoW Classic WeakAruas LoathebHeal
    Try to make LoathebHeal work around.

--- wow/WeakAuras/classic/Loatheb/LoathebHeal.lua
+++ wow/WeakAuras/classic/Loatheb/LoathebHeal.lua
@@ -1,17 +1,18 @@
 -- on-init
 local healerList = { }
-healerList['霪雨滑人'] = 1
-healerList['丑尼娜萨德星'] = 2
-healerList['犇犇萌牛丶'] = 3
-healerList['小琪琪'] = 4
-healerList['假女乃亮'] = 5
-healerList['尐酒窝'] = 6
-healerList['你的小野兽'] = 7
-healerList['上古巨神'] = 8
-healerList['一株小盆栽'] = 9
-healerList['皮皮卡'] = 10
-healerList['Pioa'] = 11
-healerList['依然活着'] = 12
+healerList['丑尼娜萨德星'] = 1
+healerList['霪雨滑人'] = 2
+healerList['小琪琪'] = 3
+healerList['依然活着'] = 4
+healerList['犇犇萌牛丶'] = 5
+healerList['假女乃亮'] = 6
+healerList['上古巨神'] = 7
+healerList['一株小盆栽'] = 8
+healerList['存款为零'] = 9
+healerList['菅田将晖'] = 10
+healerList['Yellowkitty'] = 11
+healerList['白无常'] = 12
+healerList['尐酒窝'] = 13
 
 wa_global = wa_global or { }
 wa_global.loatheb = wa_global.loatheb or { }
@@ -29,8 +30,6 @@ function(e, timestamp, event, hideCaster, sourceGUID, sourceName,
         if GetTime() - wa_global.loatheb.start < 3 then
             return false
         end
-    else
-        return false
     end
 
     if event ~= 'SPELL_AURA_APPLIED' or spellName ~= 'Corrupted Mind' then
