7e342076ab6d6a989bc05ce502e70a1f11ff81e0
jki14 <jki14wz@gmail.com>
2021-01-21 23:33:16 +0900

    Update WoW Classic WeakAuras Loatheb
    Add healerList auto compile.

--- wow/WeakAuras/classic/Loatheb/LoathebCheck.lua
+++ wow/WeakAuras/classic/Loatheb/LoathebCheck.lua
@@ -1,16 +1,17 @@
 -- on-init
 local healerList = { }
 healerList['游城哈子'] = 1
-healerList['怀旧黑莲花'] = 2
-healerList['犇犇萌牛丶'] = 3
-healerList['假女乃亮'] = 4
-healerList['霪雨滑人'] = 5
+healerList['依然活着'] = 2
+healerList['霪雨滑人'] = 3
+healerList['灵感'] = 4
+healerList['犇犇萌牛丶'] = 5
 healerList['一株小盆栽'] = 6
-healerList['颂神乐'] = 7
-healerList['你的小野兽'] = 8
+healerList['财神元宝贝'] = 7
+healerList['尐尾巴'] = 8
 healerList['皮皮卡'] = 9
-healerList['Llxx'] = 10
-healerList['盼望'] = 11
+healerList['假女乃亮'] = 10
+healerList['上古巨神'] = 11
+healerList['满大街都是'] = 12
 
 wa_global = wa_global or { }
 wa_global.loatheb = wa_global.loatheb or { }
