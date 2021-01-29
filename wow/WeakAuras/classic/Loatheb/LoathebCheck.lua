d8a048e4813cd8c47e27f81b97ce1a3fc9939f88
jki14 <jki14wz@gmail.com>
2021-01-30 00:08:03 +0900

    Update WoW Classic WeakAuras Loatheb
    Update the healerList.

--- wow/WeakAuras/classic/Loatheb/LoathebCheck.lua
+++ wow/WeakAuras/classic/Loatheb/LoathebCheck.lua
@@ -1,17 +1,18 @@
 -- on-init
 local healerList = { }
-healerList['游城哈子'] = 1
-healerList['依然活着'] = 2
-healerList['霪雨滑人'] = 3
-healerList['灵感'] = 4
-healerList['一株小盆栽'] = 5
-healerList['皮皮卡'] = 6
-healerList['满大街到处是'] = 7
-healerList['尐尾巴'] = 8
-healerList['你是真的狗'] = 9
-healerList['假女乃亮'] = 10
-healerList['哈棒豆爺'] = 11
-healerList['盼望'] = 12
+healerList['羴羴萌咩丶'] = 1
+healerList['八六'] = 2
+healerList['寇斯'] = 3
+healerList['尐酒窝'] = 4
+healerList['灬暗影萨满灬'] = 5
+healerList['大源之爹'] = 6
+healerList['闹丶'] = 7
+healerList['Promisedland'] = 8
+healerList['飞天小迷糊'] = 9
+healerList['小坏不在'] = 10
+healerList['满地插棒'] = 11
+healerList['一股烟秒成仙'] = 12
+healerList['颂神乐'] = 13
 
 wa_global = wa_global or { }
 wa_global.loatheb = wa_global.loatheb or { }
