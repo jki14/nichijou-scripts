4b275615c04cce438751253aee470c445cf92d74
jki14 <jki14wz@gmail.com>
2021-04-12 19:17:16 +0900

    Create WoW Classic WeakAuras Stormwind
    To auto mark officers.

---
+++ wow/WeakAuras/classic/Stormwind/Stormwind.lua
@@ -0,0 +1,22 @@
+-- trigger: PLAYER_TARGET_CHANGED
+function()
+    local officiers = { }
+    officiers['14438'] = 2
+    officiers['14423'] = 6
+    officiers['14439'] = 3
+
+    local guid = select(6, strsplit("-", UnitGUID('target')))
+    if guid and officiers[guid] then
+        local iconId = officiers[guid]
+        if GetRaidTargetIndex('target') ~= iconId then
+            SetRaidTarget('target', iconId)
+        end
+    end
+
+    return false
+end
+
+-- macro
+/targetexact Officer Pomeroy
+/targetexact Officer Jaxon
+/targetexact Officer Brady
