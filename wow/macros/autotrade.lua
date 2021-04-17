684a4c5d529788d74ec50c555c3d6ab6b6c693a9
jki14 <jki14wz@gmail.com>
2021-04-17 22:02:13 +0900

    Investigate auto trade macro
    AcceptTrade protected, and may only be called in response to hardware
    event.

--- wow/macros/autotrade.lua
+++ wow/macros/autotrade.lua
@@ -1,1 +1,2 @@
-/run local at=CreateFrame('Frame');at:RegisterEvent('TRADE_ACCEPT_UPDATE');at:SetScript('OnEvent',function()AcceptTrade()end);
+/run local at=CreateFrame('Frame');at:RegisterEvent('TRADE_ACCEPT_UPDATE');at:SetScript('OnEvent',function()DEFAULT_CHAT_FRAME:AddMessage('detected',1, 1, 0)end);
+/run local at=CreateFrame('Frame');at:RegisterEvent('TRADE_ACCEPT_UPDATE');at:SetScript('OnEvent',function()DEFAULT_CHAT_FRAME:AddMessage('start',1, 1, 0)AcceptTrade()DEFAULT_CHAT_FRAME:AddMessage('end',1, 1, 0)end);
