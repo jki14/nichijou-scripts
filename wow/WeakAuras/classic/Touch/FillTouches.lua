d195452982d306d6002623e247f6c734fce3d45b
jki14 <jki14wz@gmail.com>
2021-05-05 17:13:49 +0900

    Update WoW Classic WeakAruas Touch
    Predict Regrowth direct healing.

--- wow/WeakAuras/classic/Touch/FillTouches.lua
+++ wow/WeakAuras/classic/Touch/FillTouches.lua
@@ -24,23 +24,22 @@ end
 
 -- action on-init
 function fillTouches()
-  local function predictTouch(index)
-
-    local function avg(lhs, rhs)
-      return (lhs + rhs) / 2
-    end
-
-    local function GiftofNature()
-      local keyName = GetSpellInfo(17104)
-      for tabIndex = 1, GetNumTalentTabs() do
-        for i = 1, GetNumTalents(tabIndex) do
-          local name, _, _, _, spent = GetTalentInfo(tabIndex, i)
-          if name == keyName then
-            return 1.0 + spent * 0.02
-          end
+  local function getTalentSpent(spellId)
+    local keyName = GetSpellInfo(spellId)
+    for tabIndex = 1, GetNumTalentTabs() do
+      for i = 1, GetNumTalents(tabIndex) do
+        local name, _, _, _, spent = GetTalentInfo(tabIndex, i)
+        if name == keyName then
+          return spent
         end
       end
-      return 1.0
+    end
+    return 0
+  end
+
+  local function predictTouch(index, healingPower, giftOfNatureCoefficient)
+    local function avg(lhs, rhs)
+      return (lhs + rhs) / 2
     end
 
     local touches = {
@@ -61,8 +60,8 @@ function fillTouches()
       }
     }
 
-    local foo = touches.value[index] * GiftofNature()
-    local ext = GetSpellBonusHealing() * min(touches.ctime[index] / 3.5, 1.0)
+    local foo = touches.value[index] * giftOfNatureCoefficient
+    local ext = healingPower * min(touches.ctime[index] / 3.5, 1.0)
     local u20 = (touches.level[index] >= 20 and 1.0) or
                 (1.0 - ((20 - touches.level[index]) * 0.0375))
     foo = foo + ext * u20
@@ -70,16 +69,64 @@ function fillTouches()
     return foo
   end
 
+  local function predictRegrowth(index, healingPower, giftOfNatureCoefficient,
+                                 improvedRegrowthCoefficient)
+    local function avg(lhs, rhs)
+      return (lhs + rhs) / 2
+    end
+
+    local regrowthes = {
+      level = {
+                     12,              18,              24,              30,
+                     36,              42,              48,              54,
+                     60
+      },
+      value = {
+           avg(93, 107),   avg(176, 201),   avg(255, 290),   avg(336, 378),
+          avg(425, 478),   avg(534, 599),   avg(672, 751),   avg(839, 935),
+        avg(1003, 1119)
+      }
+    }
+
+    local foo = regrowthes.value[index] * giftOfNatureCoefficient
+    local ext = healingPower * 0.5 * 2.0 / 3.5
+    local u20 = (regrowthes.level[index] >= 20 and 1.0) or
+                (1.0 - ((20 - regrowthes.level[index]) * 0.0375))
+    foo = foo + ext * u20
+
+    return foo * improvedRegrowthCoefficient
+  end
+
+
+  local healingPower = GetSpellBonusHealing()
+  local giftOfNatureCoefficient = 1.0 + getTalentSpent(17104) * 0.02
+  local improvedRegrowthCoefficient = 1.0 + getTalentSpent(17074) * 0.10
+
   local touch = wa_global and wa_global.touch or { }
   touch.predicted = { }
   for i = 1, 11 do
-    table.insert(touch.predicted, math.floor(predictTouch(i)))
+    table.insert(touch.predicted,
+                 predictTouch(i, healingPower, giftOfNatureCoefficient))
+    -- print(string.format('touch %d: %d', i, touch.predicted[#touch.predicted]))
   end
+
+  local regrowth = wa_global and wa_global.regrowth or { }
+  regrowth.predicted = { }
+  for i = 1, 9 do
+    table.insert(regrowth.predicted,
+                 predictRegrowth(i, healingPower, giftOfNatureCoefficient,
+                                 improvedRegrowthCoefficient))
+    -- print(string.format('regrowth %d: %d',
+    --                     i, regrowth.predicted[#regrowth.predicted]))
+  end
+
   wa_global = wa_global or { }
   wa_global.touch = touch
+  wa_global.regrowth = regrowth
 end
 
 wa_global = wa_global or { }
 wa_global.touch = wa_global.touch or { }
+wa_global.regrowth = wa_global.regrowth or { }
 wa_global.touch.update = fillTouches
 wa_global.touch.update()
