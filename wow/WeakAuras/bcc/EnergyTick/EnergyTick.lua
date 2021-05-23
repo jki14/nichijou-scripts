df3d488d4f70f67dd35c3a7e129dbac4772ce495
jki14 <jki14wz@gmail.com>
2021-05-23 15:02:54 +0900

    Improve WoW BCC WeakAuras EnergyTick
    Prioritize the later confirmed ticks for improving the accuracy.

--- wow/WeakAuras/bcc/EnergyTick/EnergyTick.lua
+++ wow/WeakAuras/bcc/EnergyTick/EnergyTick.lua
@@ -4,22 +4,24 @@ function(a, event, unit, power)
   local currMana = UnitPower('player', 0)
   local currEnergy = UnitPower('player', 3)
   if event == 'ENERGYTICK' then
-    aura_env.inflight = 0
-    if currMana == UnitPowerMax('player', 0) then
-      flag = true
-    end
-    if 3 == GetShapeshiftForm(true) and
-        currEnergy == UnitPowerMax('player', 3) then
-      flag = true
-    end
-    if 4 == select(2, UnitClassBase('player')) and
-        currEnergy == UnitPowerMax('player', 3) then
-      flag = true
+    aura_env.inflight = math.max((aura_env.inflight or 0) - 1, 0)
+    if aura_env.inflight == 0 then
+      if currMana == UnitPowerMax('player', 0) then
+        flag = true
+      end
+      if 3 == GetShapeshiftForm(true) and
+          currEnergy == UnitPowerMax('player', 3) then
+        flag = true
+      end
+      if 4 == select(2, UnitClassBase('player')) and
+          currEnergy == UnitPowerMax('player', 3) then
+        flag = true
+      end
     end
   elseif unit == 'player' then
     if power == 'MANA' and
         11 == select(2, UnitClassBase('player')) and
-        currMana > (aura_env.lastMana or 0) + 36 then
+        currMana > (aura_env.lastMana or 0) + UnitLevel('player') / 2 then
       flag = true
     elseif power == 'ENERGY' and currEnergy > (aura_env.lastEnergy or 0) then
       flag = true
@@ -41,14 +43,11 @@ function(a, event, unit, power)
       s.duration = duration
       s.expirationTime = GetTime() + duration
       s.show = true
-      aura_env.inflight = aura_env.inflight or 0
-      if aura_env.inflight == 0 then
-        C_Timer.After(duration, function()
-          WeakAuras.ScanEvents("ENERGYTICK", true)
-        end)
-        aura_env.inflight = 1
-      end
     end
+    C_Timer.After(duration, function()
+      WeakAuras.ScanEvents("ENERGYTICK", true)
+    end)
+    aura_env.inflight = (aura_env.inflight or 0) + 1
   end
   aura_env.lastMana = currMana
   aura_env.lastEnergy = currEnergy
