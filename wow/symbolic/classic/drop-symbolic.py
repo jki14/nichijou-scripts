af91b7ce64e0ecef82b2fd037b24d642aa37d104
jki14 <jki14wz@gmail.com>
2021-05-25 18:36:57 +0900

    Improve the WoW character preference symbolic creation
    1. Fix symlink for the latest nix systems.
    2. Create the parent directories automatically.

--- wow/symbolic/classic/drop-symbolic.py
+++ wow/symbolic/classic/drop-symbolic.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
+
 
 def drop(prefix):
   for path in [os.path.join(prefix, sub) for sub in os.listdir(prefix)]:
-    if os.path.isdir(path):
-      if os.path.islink(path):
-        os.unlink(path)
+    if os.path.islink(path):
+      sys.stderr.write('islink %s\n' % path)
+      os.unlink(path)
+
 
 def accounts():
   prefix = os.path.join('WTF', 'Account')
   drop(prefix)
-  
+
+
 def characters():
   prefix = os.path.join('WTF', 'Account', '150998550#2')
   for path in [os.path.join(prefix, sub) for sub in os.listdir(prefix)]:
     if os.path.isdir(path):
       drop(path)
+
  
 def main():
   accounts()
   characters()
+
 
 if __name__ == '__main__':
   main()
