c4723004fee3c7c6125c5045acabdce62667f5be
jki14 <jki14wz@gmail.com>
2021-06-06 11:12:04 +0900

    Update WoW BCC WeakAuras CurrentStats
    Hide Fire/Forest Resistances.

--- wow/WeakAuras/bcc/CurrentStats/custom.lua
+++ wow/WeakAuras/bcc/CurrentStats/custom.lua
@@ -5,8 +5,8 @@ function()
                armor / (85 * level + armor + 400)
     dr = min(dr * 100.0, 75.0)
     local foo = 'Armor: ' .. tostring(armor) .. ' (' .. string.format('%.2f', dr) .. '%)'
-    foo = foo .. '\n' .. 'Fire Resistance: ' .. tostring(select(2, UnitResistance('player', 2)))
-    foo = foo .. '\n' .. 'Frost Resistance: ' .. tostring(select(2, UnitResistance('player', 4)))
+    -- foo = foo .. '\n' .. 'Fire Resistance: ' .. tostring(select(2, UnitResistance('player', 2)))
+    -- foo = foo .. '\n' .. 'Frost Resistance: ' .. tostring(select(2, UnitResistance('player', 4)))
     foo = foo .. '\n' .. 'Speed: ' .. string.format('%.0f', select(1, GetUnitSpeed('player')) / 7.0 * 100.0)
     return foo
 end
