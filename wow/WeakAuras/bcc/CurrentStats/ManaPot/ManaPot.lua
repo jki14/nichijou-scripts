5933b7905ec91a721851a117558eef2df7a36cef
jki14 <jki14wz@gmail.com>
2021-06-07 01:16:46 +0900

    Migrate WoW BCC WeakAuras ManaPot
    Predicting the upper bound of ManaPot restoring.

---
+++ wow/WeakAuras/bcc/CurrentStats/ManaPot/ManaPot.lua
@@ -0,0 +1,6 @@
+-- Duration Info
+function()
+  local foo = 3000 * 1.5
+  local bar = UnitPowerMax("player", 0)
+  return foo, bar, true
+end
---
+++ wow/WeakAuras/bcc/CurrentStats/ManaPot/Rune.lua
@@ -0,0 +1,6 @@
+-- Duration Info
+function()
+  local foo = 1500 * 1.5
+  local bar = UnitPowerMax("player", 0)
+  return foo, bar, true
+end
