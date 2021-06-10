410725cc82db37c1d927b2ed9358351609ba2b68
jki14 <jki14wz@gmail.com>
2021-06-11 00:21:01 +0900

    Update WoW BCC WeakAuras Touch
    Add Touch Rank 13 indicator.

---
+++ wow/WeakAuras/bcc/Touch/HTR11.lua
@@ -0,0 +1,22 @@
+-- trigger trigger
+function()
+  return UnitIsFriend("player", "target")
+end
+
+-- trigger duration
+function()
+  local index = 13
+  local foo = wa_global and wa_global.touch and wa_global.touch.predicted and
+              wa_global.touch.predicted[index] or 0
+  local bar = UnitHealthMax("target")
+  return foo, bar, true
+end
+
+-- display text-1
+function()
+  local foo = UnitHealthMax("target") - UnitHealth("target")
+  if foo > 100 then
+    return tostring(foo)
+  end
+  return ''
+end
