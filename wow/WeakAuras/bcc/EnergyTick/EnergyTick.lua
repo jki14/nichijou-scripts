373bd6eabb7ce239087a62e2b6cdd31c246c882c
jki14 <jki14wz@gmail.com>
2021-07-13 18:52:14 +0900

    Revert "Improve WoW BCC WeakAuras EnergyTick"
    This reverts commit 8fc26b8edd25d4e4dd22271f4d59019d665589fa.

--- wow/WeakAuras/bcc/EnergyTick/EnergyTick.lua
+++ wow/WeakAuras/bcc/EnergyTick/EnergyTick.lua
@@ -1,10 +1,14 @@
 -- Trigger: UNIT_POWER_FREQUENT:player,ENERGYTICK
 function(a, event, unit, power)
   local flag = false
+  local currMana = UnitPower('player', 0)
   local currEnergy = UnitPower('player', 3)
   if event == 'ENERGYTICK' then
     aura_env.inflight = math.max((aura_env.inflight or 0) - 1, 0)
     if aura_env.inflight == 0 then
+      if currMana == UnitPowerMax('player', 0) then
+        flag = true
+      end
       if 3 == GetShapeshiftForm(true) and
           currEnergy == UnitPowerMax('player', 3) then
         flag = true
@@ -15,7 +19,11 @@ function(a, event, unit, power)
       end
     end
   elseif unit == 'player' then
-    if power == 'ENERGY' and currEnergy > (aura_env.lastEnergy or 0) then
+    if power == 'MANA' and
+        11 == select(2, UnitClassBase('player')) and
+        currMana > (aura_env.lastMana or 0) + UnitLevel('player') / 2 then
+      flag = true
+    elseif power == 'ENERGY' and currEnergy > (aura_env.lastEnergy or 0) then
       flag = true
     end
   end
@@ -41,6 +49,7 @@ function(a, event, unit, power)
     end)
     aura_env.inflight = (aura_env.inflight or 0) + 1
   end
+  aura_env.lastMana = currMana
   aura_env.lastEnergy = currEnergy
   return true
 end
