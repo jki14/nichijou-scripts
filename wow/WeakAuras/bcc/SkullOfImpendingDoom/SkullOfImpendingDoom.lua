3e8291a3eed06fc5d5e02f498326d6fd12ab8145
jki14 <jki14wz@gmail.com>
2021-10-02 11:34:48 +0900

    Update WoW BCC WeakAuras SkullOfImpendingDoom
    Show SkullOfImpendingDoom is ready to use.

--- wow/WeakAuras/bcc/SkullOfImpendingDoom/SkullOfImpendingDoom.lua
+++ wow/WeakAuras/bcc/SkullOfImpendingDoom/SkullOfImpendingDoom.lua
@@ -1,6 +1,6 @@
 --custom trigger function
 function(triggers)
-    return triggers[1] or (triggers[2] and not triggers[3]) or (triggers[4] and triggers[3])
+    return triggers[1] or triggers[3] or (triggers[4] and not triggers[3])
 end
 
 -- trigger 1 && trigger 2 - run custom code:
