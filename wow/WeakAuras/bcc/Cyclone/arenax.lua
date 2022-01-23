406d651cdde208d616b565dfff3a1c8d94db1fcd
jki14 <jki14wz@gmail.com>
2022-01-23 11:10:41 +0900

    Improve WoW BCC WeakAuras Cyclone
    Further improve the performance of ArenaX, only checking debuff when it
    is necessary.

--- wow/WeakAuras/bcc/Cyclone/arenax.lua
+++ wow/WeakAuras/bcc/Cyclone/arenax.lua
@@ -13,25 +13,28 @@ function(allstates, event, ...)
     local castTime = select(4, GetSpellInfo(33786)) * 0.001
     for i = 1, 5 do
         local unit = string.format('arena%d', i)
-        local _, _, _, _, duration, expirationTime = WA_GetUnitDebuff(unit, 33786)
-        duration = duration and duration - castTime > 0 and duration - castTime or nil
-        expirationTime = duration and expirationTime and expirationTime - castTime or nil
-        local show = expirationTime and expirationTime > GetTime() or false
-        if expirationTime or allstates[unit] then
-            if not allstates[unit] or (allstates[unit].show ~= show) or (expirationTime and allstates[unit].expirationTime ~= expirationTime) then
-                allstates[unit] = {
-                    changed = true,
-                    show = show,
-                    name = unit,
-                    progressType = "timed",
-                    duration = duration,
-                    expirationTime = expirationTime,
-                    icon = 136022,
-                    tooltip = 'pending',
-                    autoHide = true,
-                    anchor = string.format('GladdyButton%d', i)
-                }
-                flag = true
+        local guid = UnitGUID(unit)
+        if guid and (destGUID == guid or (spellId ~= 33786 and allstates[unit] and allstates[unit].show)) then
+            local _, _, _, _, duration, expirationTime = WA_GetUnitDebuff(unit, 33786)
+            duration = duration and duration - castTime > 0 and duration - castTime or nil
+            expirationTime = duration and expirationTime and expirationTime - castTime or nil
+            local show = expirationTime and expirationTime > GetTime() or false
+            if show or allstates[unit] then
+                if not allstates[unit] or (allstates[unit].show ~= show) or (expirationTime and allstates[unit].expirationTime ~= expirationTime) then
+                    allstates[unit] = {
+                        changed = true,
+                        show = show,
+                        name = unit,
+                        progressType = "timed",
+                        duration = duration,
+                        expirationTime = expirationTime,
+                        icon = 136022,
+                        tooltip = 'pending',
+                        autoHide = true,
+                        anchor = string.format('GladdyButton%d', i)
+                    }
+                    flag = true
+                end
             end
         end
     end
