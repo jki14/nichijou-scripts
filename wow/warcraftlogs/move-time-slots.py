26cf7a7ef01fe84a3fd3b0c3452582109e65fd48
jki14 <jki14wz@gmail.com>
2022-02-05 11:29:24 +0900

    Improve WoW Warcraftlogs MoveTimeSlots script
    Prevent leading zeros for dates.

--- wow/warcraftlogs/move-time-slots.py
+++ wow/warcraftlogs/move-time-slots.py
@@ -7,7 +7,7 @@ def main():
             bars = input().strip().split(' ')
             dt = datetime.strptime(bars[0] + ' ' + bars[1].split('.')[0], '%m/%d %H:%M:%S')
             dt -= timedelta(minutes = 110)
-            foo = dt.strftime('%m/%d %H:%M:%S') + '.' + bars[1].split('.')[1]
+            foo = dt.strftime('$%m/$%d %H:%M:%S').replace('$0', '').replace('$', '') + '.' + bars[1].split('.')[1]
             foo = ' '.join([foo] + bars[2:])
             print(foo)
         except EOFError:
