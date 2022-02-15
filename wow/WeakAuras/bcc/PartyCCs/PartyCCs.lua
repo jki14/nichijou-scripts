600b13d1e39e8df91ba6589f08bbb9cf31b5ade0
jki14 <jki14wz@gmail.com>
2022-02-15 23:05:28 +0900

    Improve WoW BCC WeakAuras PartyCCs
    Replace the out-of-date PartyCCs with CCs-PartyX.

--- wow/WeakAuras/bcc/PartyCCs/PartyCCs.lua
+++ wow/WeakAuras/bcc/PartyCCs/PartyCCs.lua
@@ -1,4 +1,16 @@
---custom trigger function
-function(triggers)
-    return triggers[1] and (triggers[2] or triggers[3] or triggers[4] or triggers[5])
+--custom anchor
+function()
+    local unit = aura_env and aura_env.state and aura_env.state.unit or nil
+    local mapping = { }
+    mapping['player'] = 'ElvUF_PartyGroup1UnitButton1'
+    mapping['party1'] = 'ElvUF_PartyGroup1UnitButton2'
+    mapping['party2'] = 'ElvUF_PartyGroup1UnitButton3'
+    mapping['party3'] = 'ElvUF_PartyGroup1UnitButton4'
+    mapping['party4'] = 'ElvUF_PartyGroup1UnitButton5'
+    for k, v in pairs(mapping) do
+        if unit and UnitIsUnit(unit, k) then
+            return v
+        end
+    end
+    return nil
 end
