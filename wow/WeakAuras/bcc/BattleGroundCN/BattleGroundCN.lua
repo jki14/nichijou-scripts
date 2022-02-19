4d70374d092a6026a43b2717b4a5d949a42be0f0
jki14 <jki14wz@gmail.com>
2022-02-19 12:42:58 +0900

    Improve WoW BCC WeakAuras BattleGroundCN
    Add the dictionary contents for Eye of the Storm.

--- wow/WeakAuras/bcc/BattleGroundCN/BattleGroundCN.lua
+++ wow/WeakAuras/bcc/BattleGroundCN/BattleGroundCN.lua
@@ -6,7 +6,11 @@ function(e, text, _, _, _, _, _, _, _, _, _, _, guid, ...)
             ['Lumber Mill'] = '伐木场',
             ['Blacksmith'] = '铁匠铺',
             ['Gold Mine'] = '矿洞',
-            ['Stables'] = '兽栏'
+            ['Stables'] = '兽栏',
+            ['Fel Reaver Ruins'] = '魔能机甲废墟',
+            ['Blood Elf Tower'] = '血精灵塔',
+            ['Draenei Ruins'] = '德莱尼废墟',
+            ['Mage Tower'] = '法师塔',
         }
         for key, value in pairs(enCN) do
             if string.find(text, key) then
