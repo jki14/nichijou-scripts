ff218dc80cf00b57ff9071636a895c2faa22b3c8
jki14 <jki14wz@gmail.com>
2022-02-25 00:57:37 +0900

    Upgrade WoW BCC WeakAuras Feral
    Create CheatFeral.

---
+++ wow/WeakAuras/bcc/Feral/CheatFeral.lua
@@ -0,0 +1,138 @@
+-- Trigger 1: Custom / Status / Every Frame
+function()
+    if 3 ~= GetShapeshiftForm() then
+        aura_env.region:Color(0, 0, 0, 1)
+        return true
+    end
+
+    if not IsMouseButtonDown('Button4') and not IsMouseButtonDown('Button5') then
+        aura_env.region:Color(0, 0, 0, 1)
+        return true
+    end
+
+    local currts = GetTime()
+
+    local start, duration = GetSpellCooldown(27002)
+    local castts = start > 0 and start + duration - 0.1 or currts - 0.1
+
+    if currts < castts then
+        aura_env.region:Color(0, 0, 0, 1)
+        return true
+    end
+
+    local target = UnitGUID('target')
+    if not target then
+        aura_env.region:Color(0, 0, 0, 1)
+        return true
+    end
+
+    local tickts = wa_global and wa_global.feral and wa_global.feral.next_gaints or currts + 1
+    local power = UnitPower('player', 3)
+    local pwrsts = castts + math.max(select(4, GetSpellInfo(2908)) / 1000, 1.0) + 0.2
+
+    local rip_expire = wa_global and wa_global.feral and wa_global.feral.rips and wa_global.feral.rips[target] or 0
+    local mng_expire = wa_global and wa_global.feral and wa_global.feral.mangles and wa_global.feral.mangles[target] or 0
+
+    if GetComboPoints('player', 'target') == 5 then
+        if castts > rip_expire and castts + 2.2 < mng_expire and UnitHealth('target') > 432000 then
+            -- Rip
+            local cost = GetSpellPowerCost(27008)[1].cost
+            if cost <= power then
+                aura_env.region:Color(1, 0, 0, 1)
+                return true
+            elseif cost <= power + 20 and (pwrsts >= tickts or IsMouseButtonDown('Button5')) then
+                aura_env.region:Color(0, 0, 0, 1)
+                return true
+            else
+                aura_env.region:Color(1, 0, 1, 1)
+                return true
+            end
+        else
+            -- Bite
+            local cost = GetSpellPowerCost(24248)[1].cost
+            if cost <= power then
+                aura_env.region:Color(0, 1, 0, 1)
+                return true
+            elseif cost <= power + 20 then
+                aura_env.region:Color(0, 0, 0, 1)
+                return true
+            else
+                aura_env.region:Color(1, 0, 1, 1)
+                return true
+            end
+        end
+    else
+        if castts + 0.2 < mng_expire then
+            -- Shard
+            local cost = GetSpellPowerCost(27002)[1].cost
+            if cost <= power then
+                aura_env.region:Color(1, 1, 0, 1)
+                return true
+            elseif cost <= power + 20 and (pwrsts >= tickts or IsMouseButtonDown('Button5')) then
+                aura_env.region:Color(0, 0, 0, 1)
+                return true
+            else
+                aura_env.region:Color(1, 0, 1, 1)
+                return true
+            end
+        else
+            -- Mangle
+            local cost = GetSpellPowerCost(33983)[1].cost
+            if cost <= power then
+                aura_env.region:Color(0, 0, 1, 1)
+                return true
+            elseif cost <= power + 20 and (pwrsts >= tickts or IsMouseButtonDown('Button5')) then
+                aura_env.region:Color(0, 0, 0, 1)
+                return true
+            else
+                aura_env.region:Color(1, 0, 1, 1)
+                return true
+            end
+        end
+    end
+
+    aura_env.region:Color(0, 0, 0, 1)
+    return true
+end
+
+-- Trigger 2: Custom / Event: COMBAT_LOG_EVENT_UNFILTERED
+function(_, _, event, _, sourceGUID, _, _, _, destGUID, _, _, _, spellId, ...)
+    local mangleIds = {
+        [33876] = true, -- Mangle (Cat) (Rank 1)
+        [33982] = true, -- Mangle (Cat) (Rank 2)
+        [33983] = true, -- Mangle (Cat) (Rank 3)
+        [33878] = true, -- Mangle (Bear) (Rank 3)
+        [33986] = true, -- Mangle (Bear) (Rank 3)
+        [33987] = true, -- Mangle (Bear) (Rank 3)
+    }
+    if UnitGUID('player') == sourceGUID and 27008 == spellId then
+        if event == 'SPELL_AURA_APPLIED' or event == 'SPELL_DAMAGE' then
+            wa_global = wa_global or { }
+            wa_global.feral = wa_global.feral or { }
+            local rips = wa_global.feral.rips or { }
+            rips[destGUID] = GetTime() + 12
+            wa_global.feral.rips = rips
+        elseif event == 'SPELL_AURA_REMOVED' then
+            wa_global = wa_global or { }
+            wa_global.feral = wa_global.feral or { }
+            local rips = wa_global.feral.rips or { }
+            rips[destGUID] = nil
+            wa_global.feral.rips = rips
+        end
+    elseif mangleIds[spellId] then
+        if event == 'SPELL_AURA_APPLIED' or event == 'SPELL_DAMAGE' then
+            wa_global = wa_global or { }
+            wa_global.feral = wa_global.feral or { }
+            local mangles = wa_global.feral.mangles or { }
+            mangles[destGUID] = GetTime() + 12
+            wa_global.feral.mangles = mangles
+        elseif event == 'SPELL_AURA_REMOVED' then
+            wa_global = wa_global or { }
+            wa_global.feral = wa_global.feral or { }
+            local mangles = wa_global.feral.mangles or { }
+            mangles[destGUID] = nil
+            wa_global.feral.mangles = mangles
+        end
+    end
+    return true
+end
---
+++ wow/WeakAuras/bcc/Feral/CheatFeral.py
@@ -0,0 +1,76 @@
+import sys
+
+from datetime import datetime
+from keyboard import wait
+from pyautogui import pixel, press
+from time import sleep
+
+
+def distance2(lhs, rhs):
+    foo = 0
+    m = len(lhs)
+    for i in range(m):
+        foo += (lhs[i] - rhs[i]) ** 2
+    return foo
+
+
+def run():
+    cords = [
+        (0, 0, 0),      # 0
+        (255, 0, 0),    # 1
+        (0, 255, 0),    # 2
+        (255, 255, 0),  # 3
+        (0, 0, 255),    # 4
+        (255, 0, 255),  # 5
+        (0, 255, 255),  # 6
+    ]
+
+    keys = [
+        '',   # 0
+        '1',  # 1
+        '2',  # 2
+        '4',  # 3
+        'e',  # 4
+        '3',  # 5
+        '3',  # 6
+    ]
+
+    point = pixel(16, 16)
+    eps, idx = min([(distance2(point, cords[i]), i) for i in range(len(cords))])
+
+    if eps > 0:
+        sys.stdout.write('[%s] cheating paused index %d with %d epsilon.\n' % (
+                         datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                         idx, eps))
+        wait('num 0')
+        sys.stdout.write('[%s] cheating continue.\n' % (
+                         datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        sleep(0.030)
+        return False
+
+    if idx != 0:
+        press(keys[idx])
+        sys.stdout.write('[%s] Feral index %d with %d epsilon.\n' % (
+                         datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                         idx, eps))
+    sleep(0.030)
+
+
+def guard():
+    while True:
+        try:
+            run()
+        except OSError:
+            sys.stdout.write('[%s] OSError.\n' %
+                             datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            sleep(0.030)
+            pass
+
+
+def main():
+    while True:
+        guard()
+
+
+if __name__ == '__main__':
+    main()
