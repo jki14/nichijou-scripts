a1aff79012ada897ea41afe0454d94a59938caf4
jki14 <jki14wz@gmail.com>
2022-04-01 21:32:05 +0900

    Update cheatsheet for displayplacer
    Add the profile for JKI-MBA17.

--- cheatsheets/displayplacer.sh
+++ cheatsheets/displayplacer.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-# MacBook Pro 16-inch 2019
 # brew tap jakehilborn/jakehilborn && brew install displayplacer
+
+# MacBook Pro 16-inch 2019
 displayplacer "id:9449BC59-B8F6-9D46-F60A-79D17EC0BFC0 res:1792x1120 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(2616,-2000) degree:270" "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(-824,-1440) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1848,-1360) degree:90"
+
+# JKI-MBA17
+displayplacer "id:77E200AD-B0E6-0317-4BBF-F8B53DA3952C res:1440x900 color_depth:4 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-2024,-1360) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(2440,-2000) degree:270" "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(-1000,-1440) degree:0"
