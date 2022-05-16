8573850dd03b454b147c1352aef441420fd2be96
jki14 <jki14wz@gmail.com>
2022-05-16 22:28:28 +0900

    Update WoW BCC WeakAuras MostDamaged
    1. Move EredarTwins into MostDamaged.
    2. Delay the focus target check for the Conflagration.

--- wow/WeakAuras/bcc/MostDamaged/EredarTwins.lua
+++ wow/WeakAuras/bcc/MostDamaged/EredarTwins.lua
@@ -1,10 +1,14 @@
--- Trigger State Updater: Event(s): COMBAT_LOG_EVENT_UNFILTERED
-function(allstates, _, _, event, _, _, _, _, _, destGUID, _, _, _, spellId, ...)
-    if 'SPELL_CAST_START' == event and 45342 == spellId then
-        destGUID = destGUID or UnitGUID('focustarget')
+-- Trigger State Updater: Event(s): COMBAT_LOG_EVENT_UNFILTERED, MD_CONFLAGRATION
+function(allstates, event, _, subevent, _, _, _, _, _, _, _, _, _, spellId, ...)
+    if 'COMBAT_LOG_EVENT_UNFILTERED' == event and 'SPELL_CAST_START' == subevent and 45342 == spellId then
+        C_Timer.After(0.2, function()
+            WeakAuras.ScanEvents('MD_CONFLAGRATION', true)
+        end)
+    elseif 'MD_CONFLAGRATION' == event then
+        local destGUID = UnitGUID('focustarget')
         local frame = wa_global and wa_global.mostDamaged and wa_global.mostDamaged.guidframes and wa_global.mostDamaged.guidframes[destGUID] or nil
         if frame then
-            local duration = 3.5
+            local duration = 3.3
             allstates[destGUID] = {
                 changed = true,
                 show = true,
