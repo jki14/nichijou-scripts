fdfb66651e733fc23bfc135d33fda6ef89a1172d
jki14 <jki14wz@gmail.com>
2022-05-20 21:36:39 +0900

    Create WoW Rsync Script
    Make the push bash script to sync configurations with rsync to kaguya.

---
+++ wow/rsync/push.sh
@@ -0,0 +1,3 @@
+rsync -Rchrvz -e 'ssh -p 3122' WTF jki14@kaguya:~/archives/wow/bcc
+rsync -Rchrvz -e 'ssh -p 3122' Interface jki14@kaguya:~/archives/wow/bcc
+rsync -Rchrvz -e 'ssh -p 3122' CustomMedias jki14@kaguya:~/archives/wow/bcc
