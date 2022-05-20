2c88ff20111be340adf560b92b0b4db1891b610c
jki14 <jki14wz@gmail.com>
2022-05-20 21:49:40 +0900

    Update WoW Rsync Script
    Improve the push script with adding the bash shebang.

--- wow/rsync/push.sh
+++ wow/rsync/push.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 rsync -Rchrvz -e 'ssh -p 3122' WTF jki14@kaguya:~/archives/wow/bcc
 rsync -Rchrvz -e 'ssh -p 3122' Interface jki14@kaguya:~/archives/wow/bcc
 rsync -Rchrvz -e 'ssh -p 3122' CustomMedias jki14@kaguya:~/archives/wow/bcc
