5af6c76763d88c713fbf814d31a89da451b38c28
jki14 <jki14wz@gmail.com>
2022-05-21 01:19:48 +0900

    Create WoW Pyautogui Script 2
    Keep pressing 2 every eight to sixteen seconds.

---
+++ wow/Pyautogui/2.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+from datetime import datetime
+from pyautogui import press
+from time import sleep
+
+
+def urandom32(low, hig):
+    foo = os.urandom(4).hex()
+    foo = int(foo, 16)
+    foo %= hig - low + 1
+    foo += low
+    return foo
+
+
+def shoot(key):
+    press(key)
+    # sys.stdout.write('[%s] %s pressed\n'
+    #                  % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), key))
+    # sys.stdout.flush()
+
+
+def main():
+    try:
+        while True:
+            sleep(urandom32(8, 16) * 0.01)
+            shoot('2')
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
