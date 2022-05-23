614d578fd26aec3eb01d536f11914002f8bab69d
jki14 <jki14wz@gmail.com>
2022-05-23 14:14:50 +0900

    Update cheatsheet for displayplacer
    Add the profile for JKI-MBA17 (without internal).

--- cheatsheets/displayplacer.sh
+++ cheatsheets/displayplacer.sh
@@ -10,3 +10,6 @@ displayplacer "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color
 
 # JKI-MBA17
 displayplacer "id:77E200AD-B0E6-0317-4BBF-F8B53DA3952C res:1440x900 color_depth:4 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-2024,-1360) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(2440,-2000) degree:270" "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(-1000,-1440) degree:0"
+
+# JKI-MBA17 (without internal)
+displayplacer "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1024,80) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(3440,-560) degree:270"
