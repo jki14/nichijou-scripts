e7e98067457b230f391141bdaf7162a1f8df1a59
jki14 <jki14wz@gmail.com>
2022-05-27 23:02:51 +0900

    Create WoW BCC WeakAuras Blink
    Show enemies' cooldowns on the their nameplates.

---
+++ wow/WeakAuras/bcc/Blink/Blink.lua
@@ -0,0 +1,54 @@
+--trigger state updater: event(s): NAME_PLATE_UNIT_ADDED, NAME_PLATE_UNIT_REMOVED, UNIT_SPELLCAST_SUCCEEDED
+function(allstates, event, ...)
+    if event == 'NAME_PLATE_UNIT_ADDED' or event == 'NAME_PLATE_UNIT_REMOVED' then
+        local unit = ...
+        local guid = UnitGUID(unit)
+        if allstates[guid] then
+            allstates[guid].changed = true
+            allstates[guid].unit = event == 'NAME_PLATE_UNIT_ADDED' and unit or nil
+        end
+        wa_global = wa_global or { }
+        wa_global.nameplates = wa_global.nameplates or { }
+        wa_global.nameplates[guid] = event == 'NAME_PLATE_UNIT_ADDED' and unit or nil
+    end
+
+    if event == 'UNIT_SPELLCAST_SUCCEEDED' then
+        local spells = {}
+        spells[5401] = {
+            cooldown = 15,
+            icon = 136048
+        }
+        local unit, castGUID, spellID = ...
+        if spells[spellID] then
+            wa_global = wa_global or { }
+            wa_global.nameplates = wa_global.nameplates or { }
+            local guid = UnitGUID(unit)
+            allstates[guid] = {
+                changed = true,
+                show = true,
+                progressType = 'timed',
+                duration = spells[spellID].cooldown,
+                expirationTime = GetTime() + spells[spellID].cooldown,
+                name = unit,
+                icon = spells[spellID].icon,
+                autoHide = true,
+                -- appendix
+                unit = wa_global.nameplates[guid] and UnitIsUnit(wa_global.nameplates[guid], unit) and wa_global.nameplates[guid] or nil
+            }
+        end
+    end
+
+    return true
+end
+
+
+--custom text
+function()
+    return aura_env and aura_env.state and aura_env.state.name or ''
+end
+
+
+--custom anchor
+function()
+    return aura_env.state and aura_env.state.unit and C_NamePlate.GetNamePlateForUnit(aura_env.state.unit) or nil
+end
