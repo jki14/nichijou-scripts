5cbe5860d2e4e62ced4fd6c2092b925b430356db
jki14 <jki14wz@gmail.com>
2022-05-28 11:23:11 +0900

    Update WoW BCC WeakAuras Blink
    1. Start to track Blink, Shadowstep, and Intercept.
    2. Expand the tracking range with including COMBAT_LOG_EVENT_UNFILTER.
    3. Only takes the first fire for batch engaging.

--- wow/WeakAuras/bcc/Blink/Blink.lua
+++ wow/WeakAuras/bcc/Blink/Blink.lua
@@ -1,4 +1,4 @@
---trigger state updater: event(s): NAME_PLATE_UNIT_ADDED, NAME_PLATE_UNIT_REMOVED, UNIT_SPELLCAST_SUCCEEDED
+--trigger state updater: event(s): NAME_PLATE_UNIT_ADDED, NAME_PLATE_UNIT_REMOVED, UNIT_SPELLCAST_SUCCEEDED, COMBAT_LOG_EVENT_UNFILTERED
 function(allstates, event, ...)
     if event == 'NAME_PLATE_UNIT_ADDED' or event == 'NAME_PLATE_UNIT_REMOVED' then
         local unit = ...
@@ -10,31 +10,48 @@ function(allstates, event, ...)
         wa_global = wa_global or { }
         wa_global.nameplates = wa_global.nameplates or { }
         wa_global.nameplates[guid] = event == 'NAME_PLATE_UNIT_ADDED' and unit or nil
-    end
-
-    if event == 'UNIT_SPELLCAST_SUCCEEDED' then
+    elseif event == 'UNIT_SPELLCAST_SUCCEEDED' or select(2, ...) == 'SPELL_CAST_SUCCESS' then
         local spells = {}
+        -- Lizard Bolt
         spells[5401] = {
             cooldown = 15,
             icon = 136048
         }
-        local unit, castGUID, spellID = ...
+        -- Blink
+        spells[1953] = {
+            cooldown = 15,
+            icon = 135736
+        }
+        -- Shadowstep
+        spells[36554] = {
+            cooldown = 30,
+            icon = 132303
+        }
+        -- Intercept
+        spells[25275] = {
+            cooldown = 15,
+            icon = 132307
+        }
+        local guid = event == 'UNIT_SPELLCAST_SUCCEEDED' and UnitGUID(select(1, ...)) or select(4, ...)
+        local spellID = event == 'UNIT_SPELLCAST_SUCCEEDED' and select(3, ...) or select(12, ...)
         if spells[spellID] then
             wa_global = wa_global or { }
             wa_global.nameplates = wa_global.nameplates or { }
-            local guid = UnitGUID(unit)
-            allstates[guid] = {
-                changed = true,
-                show = true,
-                progressType = 'timed',
-                duration = spells[spellID].cooldown,
-                expirationTime = GetTime() + spells[spellID].cooldown,
-                name = unit,
-                icon = spells[spellID].icon,
-                autoHide = true,
-                -- appendix
-                unit = wa_global.nameplates[guid] and UnitIsUnit(wa_global.nameplates[guid], unit) and wa_global.nameplates[guid] or nil
-            }
+            local timestamp = GetTime()
+            if not allstates[guid] or allstates[guid].expirationTime - 0.2 < timestamp then
+                allstates[guid] = {
+                    changed = true,
+                    show = true,
+                    progressType = 'timed',
+                    duration = spells[spellID].cooldown,
+                    expirationTime = timestamp + spells[spellID].cooldown,
+                    name = guid,
+                    icon = spells[spellID].icon,
+                    autoHide = true,
+                    -- appendix
+                    unit = wa_global.nameplates[guid] and UnitGUID(wa_global.nameplates[guid]) == guid and wa_global.nameplates[guid] or nil
+                }
+            end
         end
     end
 
