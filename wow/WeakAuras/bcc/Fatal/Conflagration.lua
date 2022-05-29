e3b46d2a4ebadebd7182107b7f64e9488437ebe9
jki14 <jki14wz@gmail.com>
2022-05-29 09:30:45 +0900

    Update WoW BCC WeakAuras Fatal
    Tracking Conflagration for Eredar Twins fight no longer require certain
    focus set.

--- wow/WeakAuras/bcc/Fatal/Conflagration.lua
+++ wow/WeakAuras/bcc/Fatal/Conflagration.lua
@@ -1,4 +1,15 @@
--- trigger combination: custom function
-function(triggers)
-    return not triggers[1] or (triggers[1] and triggers[2] and triggers[3])
+-- Event(s): UNIT_SPELLCAST_START, F_CONFLAGRATION
+function(event, ...)
+    if 'UNIT_SPELLCAST_START' == event then
+        local unit, _, spellId = ...
+        if spellId == 45342 or spellId == 5401 then
+            C_Timer.After(0.1, function()
+                WeakAuras.ScanEvents('F_CONFLAGRATION', unit .. 'target')
+            end)
+        end
+    else
+        local dest = ...
+        return UnitIsUnit('player', dest)
+    end
+    return false
 end
