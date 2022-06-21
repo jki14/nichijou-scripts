90a9f5af562bfec1d645ec10eb4d278427c98d3c
jki14 <jki14wz@gmail.com>
2022-06-21 21:01:11 +0900

    Create WoW BCC WeakAuras Stormwind
    Auto add raid marks on anti-stealth guards.

---
+++ wow/WeakAuras/bcc/Stormwind/Stormwind.lua
@@ -0,0 +1,28 @@
+-- trigger: PLAYER_TARGET_CHANGED
+function()
+    local officiers = { }
+    officiers['14438'] = 2
+    officiers['14423'] = 6
+    officiers['14439'] = 3
+    officiers['14363'] = 2
+    officiers['14367'] = 6
+    officiers['14365'] = 3
+
+    local guid = select(6, strsplit("-", UnitGUID('target')))
+    if guid and officiers[guid] then
+        local iconId = officiers[guid]
+        if GetRaidTargetIndex('target') ~= iconId then
+            SetRaidTarget('target', iconId)
+        end
+    end
+
+    return false
+end
+
+-- macro
+/targetexact Officer Brady
+/targetexact Officer Jaxon
+/targetexact Officer Pomeroy
+/targetexact Thief Catcher Farmountain
+/targetexact Thief Catcher Thunderbrew
+/targetexact Thief Catcher Shadowdelve
