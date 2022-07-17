92f2145fca64732bb74701974da0b3e5527e6f87
jki14 <jki14wz@gmail.com>
2022-07-17 17:57:09 +0900

    Update WoW BCC WeakAuras Improved FaerieFire
    Trigger at the encounter beginning.

--- wow/WeakAuras/bcc/Resto/Improved-FaerieFire.lua
+++ wow/WeakAuras/bcc/Resto/Improved-FaerieFire.lua
@@ -1,16 +1,46 @@
--- Trigger: Custom / Event: COMBAT_LOG_EVENT_UNFILTERED
-function(_, _, event, _, sourceGUID, _, _, _, destGUID, _, _, _, spellId, ...)
-    if UnitGUID('player') == sourceGUID and 26993 == spellId then
-        if event == 'SPELL_AURA_APPLIED' or event == 'SPELL_AURA_REFRESH' then
-            return true
+-- Trigger State Updater: Event(s): COMBAT_LOG_EVENT_UNFILTERED, ENCOUNTER_START
+-- PLAYER_REGEN_DISABLED
+function(allstates, event, ...)
+    if event == 'COMBAT_LOG_EVENT_UNFILTERED' then
+        local _, subevent, _, sourceGUID, _, _, _, destGUID, _, _, _, spellId = ...
+        if UnitGUID('player') == sourceGUID and 26993 == spellId then
+            if subevent == 'SPELL_AURA_APPLIED' or subevent == 'SPELL_AURA_REFRESH' then
+                allstates[''] = {
+                    changed = true,
+                    show = true,
+                    name = 'ImprovedFaerieFire',
+                    progressType = 'timed',
+                    duration = 47,
+                    expirationTime = GetTime() + 47,
+                    autoHide = true
+                }
+                return true
+            end
         end
+    else
+        allstates[''] = {
+            changed = true,
+            show = true,
+            name = 'ImprovedFaerieFire',
+            progressType = 'timed',
+            duration = 14,
+            expirationTime = GetTime() + 14,
+            autoHide = true
+        }
+        return true
     end
     return false
 end
 
+-- Custom Variables
+{
+    expirationTime = true,
+    duration = true
+}
+
 -- Custom Text
 function()
     local p = aura_env.state.expirationTime and math.floor(aura_env.state.expirationTime - GetTime() - 7) or -1
-    p = p >= 0 and tostring(p) or ''
+    p = tostring(p)
     return p
 end
