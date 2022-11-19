e6735d843d55c98d6de275bd71c3ec0f3b34b282
jki14 <jki14wz@gmail.com>
2022-11-19 09:13:18 +0900

    Update WoW WotLK WeakAuras DalaranMount
    Drop false-positive callings.

--- wow/WeakAuras/wotlkc/DalaranMount/DalaranMount.lua
+++ wow/WeakAuras/wotlkc/DalaranMount/DalaranMount.lua
@@ -1,10 +1,16 @@
--- Trigger: UI_ERROR_MESSAGE
-function (event, code, msg)
-    wa_global = wa_global or { }
-    local lastts = wa_global.delaran_mount or 0
-    if code == 50 and lastts < GetTime() - 2.0 then
-        wa_global.delaran_mount = GetTime()
-        CallCompanion('MOUNT', 3)
+-- Trigger / Custom / Event(s): UNIT_SPELLCAST_FAILED
+function(_, unit, _, spellId)
+    local flying = {
+        [49193] = 'Vengeful Nether Drake',
+        [58615] = 'Brutal Nether Drake',
+    }
+    if 'player' == unit and spellId and flying[spellId] then
+        wa_global = wa_global or { }
+        local lastts = wa_global.delaran_mount or 0
+        if lastts < GetTime() - 2.0 then
+            wa_global.delaran_mount = GetTime()
+            CallCompanion('MOUNT', 3)
+        end
     end
     return false
 end
