c8f72626da08486c49047d23f8c85e03dfdfcca0
jki14 <jki14wz@gmail.com>
2022-12-08 23:44:09 +0900

    Update WoW WotLK WeakAuras RelicRotate
    Change the preferred relic for boss fights to Idol of Worship.

--- wow/WeakAuras/wotlkc/RelicRotate/RelicRotate.lua
+++ wow/WeakAuras/wotlkc/RelicRotate/RelicRotate.lua
@@ -32,12 +32,12 @@ function(event, ...)
     elseif 'PLAYER_REGEN_ENABLED' == event then
         if -1 == UnitLevel('target') then
             C_Timer.After(0.4, function()
-                rotate(40713, 4)
-            end) -- Idol of the Ravenous Beast
+                rotate(39757, 4)
+            end) -- Idol of Worship
         end
     else
         if not InCombatLockdown() and -1 == UnitLevel('target') then
-            rotate(40713, 0) -- Idol of the Ravenous Beast
+            rotate(39757, 0) -- Idol of Worship
         end
     end
 
