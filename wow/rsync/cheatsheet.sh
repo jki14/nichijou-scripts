44705f2dbb6d84278ba57117aa1c5ffd9ae006e3
jki14 <jki14wz@gmail.com>
2022-12-29 09:26:43 +0900

    Update WoW Rsync
    Add cheatsheet for taking over profiles.

---
+++ wow/rsync/cheatsheet.sh
@@ -0,0 +1,4 @@
+sed -i "s/刚到底/Kugraza/g" `grep 刚到底 -rl .`
+sed -i "s/Kugraza - 比格沃斯/Kugraza - Classic PTR Realm 1/g" `grep 'Kugraza - 比格沃斯' -rl .`
+sed -i "s/Kugraza-比格沃斯/Kugraza-Classic PTR Realm 1/g" `grep 'Kugraza-比格沃斯' -rl .`
+sed -i "s/Kugraza比格沃斯/KugrazaClassic PTR Realm 1/g" `grep 'Kugraza比格沃斯' -rl .`
