2762ec5a0b42d608c259124b598431d43626464a
jki14 <jki14wz@gmail.com>
2023-02-01 23:03:23 +0900

    Update WoW WotLK WeakAuras DalaranMount
    Improve the DalaranMount for 逐風者 characters.

--- wow/WeakAuras/wotlkc/DalaranMount/DalaranMount.lua
+++ wow/WeakAuras/wotlkc/DalaranMount/DalaranMount.lua
@@ -3,13 +3,26 @@ function(_, unit, _, spellId)
     local flying = {
         [49193] = 'Vengeful Nether Drake',
         [58615] = 'Brutal Nether Drake',
+        [59569] = 'Bronze Drake',
     }
     if 'player' == unit and spellId and flying[spellId] then
         wa_global = wa_global or { }
         local lastts = wa_global.delaran_mount or 0
         if lastts < GetTime() - 2.0 then
+            local offset = wa_global and wa_global.ground_mount or nil
+            if not offset then
+                local num = GetNumCompanions('MOUNT')
+                for i = 1, num + 1 do
+                    if i == num + 1 or 'Great White Kodo' == select(2, GetCompanionInfo('MOUNT', i)) then
+                        offset = i ~= num + 1 and i or 1
+                        wa_global = wa_global or { }
+                        wa_global.ground_mount = offset
+                        break
+                    end
+                end
+            end
             wa_global.delaran_mount = GetTime()
-            CallCompanion('MOUNT', 3)
+            CallCompanion('MOUNT', offset)
         end
     end
     return false
