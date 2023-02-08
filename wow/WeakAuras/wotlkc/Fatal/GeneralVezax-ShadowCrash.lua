3f109fa3dc2880ef638c3c5c838779fab16ba3fb
jki14 <jki14wz@gmail.com>
2023-02-09 00:36:46 +0900

    Update WoW WotLK WeakAuras Fatal
    Improve the advance tracker for General Vezax - Shadow Crash.

--- wow/WeakAuras/wotlkc/Fatal/GeneralVezax-ShadowCrash.lua
+++ wow/WeakAuras/wotlkc/Fatal/GeneralVezax-ShadowCrash.lua
@@ -11,8 +11,8 @@ function(event, ...)
     end
     if remain > 0 then
         remain = remain - 1
-        local ft = UnitHealthMax('focustarget') or 0
-        if UnitHealthMax('focustarget') < 30000 then
+        local ft = UnitHealthMax('focustarget') or 99999
+        if ft < 30000 then
             local min_range, _ = WeakAuras.GetRange('focustarget')
             DEFAULT_CHAT_FRAME:AddMessage('|cFFAD7FA8[FATAL_SHADOWCRASH] ' .. tostring(min_range))
             return not min_range or min_range <= 10
