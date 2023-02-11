c4bf263c091d0db802405a3e1f5df9e9598ae43e
jki14 <jki14wz@gmail.com>
2023-02-11 17:09:16 +0900

    Update WoW WotLK WeakAuras Fatal
    Add the advance tracker for YoggSaron - Malady of the Mind.

---
+++ wow/WeakAuras/wotlkc/Fatal/YoggSaron-MaladyOfTheMind.lua
@@ -0,0 +1,39 @@
+-- Trigger 1 / Custom / Event(s): COMBAT_LOG_EVENT_UNFILTERED, FATAL_MINDMALADY
+function(event, ...)
+    local unit, remain = nil, 0
+    if event == 'COMBAT_LOG_EVENT_UNFILTERED' then
+        local _, subevent, _, _, _, _, _, _, destName, _, _, spellId = ...
+        if subevent == 'SPELL_AURA_APPLIED' then
+            local debuffs = {
+                -- [48441] = 40, -- Rejuvenation
+                [63830] = 40, -- Malady of the Mind
+                [63881] = 40, -- Malady of the Mind
+            }
+            unit = destName
+            remain = spellId and debuffs[spellId] or 0
+        end
+    else
+        unit, remain = ...
+    end
+    if unit and remain > 0 then
+        C_Timer.After(0.1, function()
+            WeakAuras.ScanEvents('FATAL_MINDMALADY', unit, remain - 1)
+        end)
+        local min_range, _ = WeakAuras.GetRange(unit)
+        if remain % 10 == 0 then
+            DEFAULT_CHAT_FRAME:AddMessage('|cFFAD7FA8[FATAL_MINDMALADY] ' .. unit .. ' (' .. tostring(min_range) .. ', ' .. tostring(remain) .. ')')
+        end
+        return not min_range or min_range <= 10
+    end
+    return false
+end
+
+-- Trigger 1 / Custom Untrigger
+function(event, ...)
+    if event == 'FATAL_MINDMALADY' then
+        local unit, remain = ...
+        local min_range, _ = WeakAuras.GetRange(unit)
+        return remain == 0 or (min_range and min_range > 10)
+    end
+    return false
+end
