8e91477a9c173db59ae51eabca685986eeb50720
jki14 <jki14wz@gmail.com>
2023-04-06 23:42:51 +0900

    Update WoW WotLK Classic WeakAura ValanyrsFragment
    1. Only greed when Fragment is found in the bag.
    2. Support Alchemist's Cache greed.

--- wow/WeakAuras/wotlkc/ValanyrsFragment/ValanyrsFragment.lua
+++ wow/WeakAuras/wotlkc/ValanyrsFragment/ValanyrsFragment.lua
@@ -1,8 +1,14 @@
 -- Trigger-1 / Custom / Event: START_LOOT_ROLL
 function(e, id, ...)
     if e == 'START_LOOT_ROLL' then
-        local _, _, _, quality = GetLootRollItemInfo(id)
-        if quality == 5 then
+        local _, name, _, quality = GetLootRollItemInfo(id)
+        -- DEFAULT_CHAT_FRAME:AddMessage('|cFFFF8000[ValanyrsFragment] texture = ' .. texture .. ', name = ' .. name .. '.')
+        if name == GetItemInfo(45038) then
+            -- Fragment of Val'anyr
+            local num = GetItemCount(45038)
+            RollOnLoot(id, num > 0 and num < 30 and 2 or 0)
+        elseif name == GetItemInfo(46110) then
+            -- Alchemist's Cache
             RollOnLoot(id, 2)
         else
             RollOnLoot(id, 0)
