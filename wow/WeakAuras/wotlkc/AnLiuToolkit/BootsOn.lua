1e0372a195ff83bcb8c96f5f4edbc24ddf5212e2
jki14 <jki14wz@gmail.com>
2023-04-27 20:37:47 +0900

    Update WoW WotLK Classic WeakAuras AnLiuToolkit
    No longer switch on the boots for transient engagement.

--- wow/WeakAuras/wotlkc/AnLiuToolkit/BootsOn.lua
+++ wow/WeakAuras/wotlkc/AnLiuToolkit/BootsOn.lua
@@ -1,4 +1,4 @@
--- Trigger 1 / Custom / Event: ENCOUNTER_END
+-- Trigger 1 / Custom / Event: ENCOUNTER_START, ENCOUNTER_END
 function(event, ...)
     local bootsLeather = 23824
     local bootsCloth = 35581
@@ -26,15 +26,32 @@ function(event, ...)
         }
         DEFAULT_CHAT_FRAME:AddMessage('|cFFFFF468[AnLiuToolkits-BootsOn] Encounter ' .. tostring(encounterId) .. (success and ' success.' or ' failed.'))
         if allows[encounterId] and success then
-            if GetItemCount(bootsLeather) > 0 then
-                C_Timer.After(0.4, function()
-                    switchOn(bootsLeather, 8)
-                end)
-            elseif GetItemCount(bootsCloth) > 0 then
-                C_Timer.After(0.4, function()
-                    switchOn(bootsCloth, 8)
-                end)
+            local start_time = wa_global and wa_global.anliutoolkit and wa_global.anliutoolkit.encounter_start or 2147483647
+            local duration = GetTime() - start_time
+            DEFAULT_CHAT_FRAME:AddMessage('|cFFFFF468[AnLiuToolkits-BootsOn] Encounter duration = ' .. tostring(duration) .. ' secs.')
+            if duration >= 120 and duration <= 660 then
+                if GetItemCount(bootsLeather) > 0 then
+                    C_Timer.After(0.4, function()
+                        switchOn(bootsLeather, 8)
+                    end)
+                elseif GetItemCount(bootsCloth) > 0 then
+                    C_Timer.After(0.4, function()
+                        switchOn(bootsCloth, 8)
+                    end)
+                end
             end
+        end
+    elseif 'ENCOUNTER_START' == event then
+        local allows = {
+            [1942] = 'Hydromancer Thespia',
+            [754] = 'Mimiron',
+            [757] = 'Algalon the Observer',
+        }
+        local encounterId, encounterName, difficultyId, groupSize = ...
+        if allows[encounterId] then
+            wa_global = wa_global or { }
+            wa_global.anliutoolkit = wa_global.anliutoolkit or { }
+            wa_global.anliutoolkit.encounter_start = GetTime()
         end
     end
 
---
+++ wow/WeakAuras/wotlkc/Fatal/YoggSaron-SarasFervor.lua
@@ -0,0 +1,27 @@
+-- Trigger 1 / Custom / Event(s): UNIT_SPELLCAST_SUCCEEDED, FATAL_SHADOWCRASH
+function(event, ...)
+    local remain = 0
+    if event == 'UNIT_SPELLCAST_SUCCEEDED' then
+        local _, _, spellId = ...
+        if spellId == 62660 then
+            remain = 14
+        end
+    else
+        remain = ...
+    end
+    if remain > 0 then
+        remain = remain - 1
+        local ft = UnitHealthMax('focustarget') or 0
+        if ft < 30000 then
+            local min_range, _ = WeakAuras.GetRange('focustarget')
+            DEFAULT_CHAT_FRAME:AddMessage('|cFFAD7FA8[FATAL_SHADOWCRASH] ' .. tostring(min_range))
+            return not min_range or min_range <= 10
+        end
+        if remain > 0 then
+            C_Timer.After(0.05, function()
+                WeakAuras.ScanEvents('FATAL_SHADOWCRASH', remain)
+            end)
+        end
+    end
+    return false
+end
