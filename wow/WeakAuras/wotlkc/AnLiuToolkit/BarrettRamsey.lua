06953f2a04a851d578b1553c6996bf0ea543365e
jki14 <jki14wz@gmail.com>
2023-05-22 19:28:47 +0900

    Update WoW WotLK Classic WeakAuras AnLiuToolkit
    1. Disable the first boss auto starting for BarrettRamsey.
    2. Add the notifications for the Shields of the VT fight.

--- wow/WeakAuras/wotlkc/AnLiuToolkit/BarrettRamsey.lua
+++ wow/WeakAuras/wotlkc/AnLiuToolkit/BarrettRamsey.lua
@@ -1,8 +1,11 @@
 --trigger: GOSSIP_SHOW
 function()
-    SelectGossipOption(1)
-    SelectGossipOption(1)
-    SelectGossipOption(1)
-    SelectGossipOption(1)
+    local npcId = select(6, strsplit('-', UnitGUID('target') or ''))
+    if npcId ~= '35909' then
+        SelectGossipOption(1)
+        SelectGossipOption(1)
+        SelectGossipOption(1)
+        SelectGossipOption(1)
+    end
     return false
 end
---
+++ wow/WeakAuras/wotlkc/AnLiuToolkit/VT-Shield.lua
@@ -0,0 +1,4 @@
+-- Trigger Combination Custom Function
+function(triggers)
+    return triggers[2] and triggers[3] and not triggers[4]
+end
