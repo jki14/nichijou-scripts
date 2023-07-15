d54868dabe7d6928e45d23877843fd84b6ed6d77
jki14 <jki14wz@gmail.com>
2023-07-15 21:48:30 +0900

    Update WoW WotLK Classic WeakAuras BarrettRamsey
    1. Ignore the Jeeves.
    2. Refresh the release string.

--- wow/WeakAuras/wotlkc/BarrettRamsey/BarrettRamsey.lua
+++ wow/WeakAuras/wotlkc/BarrettRamsey/BarrettRamsey.lua
@@ -1,7 +1,7 @@
 --trigger: GOSSIP_SHOW
 function()
     local npcId = select(6, strsplit('-', UnitGUID('target') or ''))
-    if npcId ~= '35909' then
+    if npcId ~= '35909' and npcId ~= '35642' then
         SelectGossipOption(1)
         SelectGossipOption(1)
         SelectGossipOption(1)
