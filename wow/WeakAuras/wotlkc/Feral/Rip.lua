4b0eb13aeef183dfb7350414fa45dc7244813a3f
jki14 <jki14wz@gmail.com>
2023-07-25 21:04:05 +0900

    Update WoW WotLK Classic WeakAuras Feral
    1. Fix Glyph of Shreds indicator for the Rip aura.
    2. Refresh the release string.

--- wow/WeakAuras/wotlkc/Feral/Rip.lua
+++ wow/WeakAuras/wotlkc/Feral/Rip.lua
@@ -1,48 +1,16 @@
--- Trigger 2 / Custom / Event(s): CLEU:SPELL_AURA_APPLIED, CLEU:SPELL_AURA_REFRESH, CLEU:SPELL_AURA_REMOVED, CLEU:SPELL_DAMAGE, PLAYER_TARGET_CHANGED
-function(event, ...)
-    local changed = false
-    if 'COMBAT_LOG_EVENT_UNFILTERED' == event then
-        local _, subevent, _, sourceGuid, _, _, _, guid, _, _, _, spellId = ...
-        if UnitGUID('player') == sourceGuid then
-            if 49800 == spellId then
-                if subevent == 'SPELL_AURA_APPLIED' or subevent == 'SPELL_AURA_REFRESH' then
-                    wa_global = wa_global or { }
-                    wa_global.shreds = wa_global.shreds or { }
-                    wa_global.shreds[guid] = 0
-                    changed = true
-                elseif subevent == 'SPELL_AURA_REMOVED' then
-                    wa_global = wa_global or { }
-                    wa_global.shreds = wa_global.shreds or { }
-                    wa_global.shreds[guid] = nil
-                    changed = true
-                end
-            elseif 48572 == spellId then
-                if subevent == 'SPELL_DAMAGE' then
-                    wa_global = wa_global or { }
-                    wa_global.shreds = wa_global.shreds or { }
-                    wa_global.shreds[guid] = wa_global.shreds[guid] and wa_global.shreds[guid] + 1 or nil
-                    changed = true
-                end
-            end
-        end
-    end
-
-    if changed or 'PLAYER_TARGET_CHANGED' == event then
-        local guid = UnitGUID('target')
-        if guid then
-            local stacks = wa_global and wa_global.shreds and wa_global.shreds[guid] or 0
-            for i, subregion in ipairs(aura_env.region.subRegions) do
-                if subregion.type == 'subborder' then
-                    if stacks == 0 then
-                        subregion:SetBorderColor(173 / 255, 127 / 255, 168 / 255, 1)
-                    elseif stacks == 1 then
-                        subregion:SetBorderColor(113 / 255, 159 / 255, 207 / 255, 1)
-                    elseif stacks == 2 then
-                        subregion:SetBorderColor(154 / 255, 226 / 255, 52 / 255, 1)
-                    else
-                        subregion:SetBorderColor(1, 1, 1, 1)
-                    end
-                end
+-- Trigger 2 / Custom / Event(s): FERAL_GSHREDS_UPDATED
+function()
+    local stacks = wa_global and wa_global.feralRaid and wa_global.feralRaid.gshreds or 0
+    for i, subregion in ipairs(aura_env.region.subRegions) do
+        if subregion.type == 'subborder' then
+            if stacks == 3 then
+                subregion:SetBorderColor(173 / 255, 127 / 255, 168 / 255, 1)
+            elseif stacks == 2 then
+                subregion:SetBorderColor(113 / 255, 159 / 255, 207 / 255, 1)
+            elseif stacks == 1 then
+                subregion:SetBorderColor(154 / 255, 226 / 255, 52 / 255, 1)
+            else
+                subregion:SetBorderColor(1, 1, 1, 1)
             end
         end
     end
