6e8786299a5f27a47ec81f56838e134759bfa449
jki14 <jki14wz@gmail.com>
2023-09-15 17:49:53 +0800

    Update cheatsheet for displayplacer
    Add the profile for MacBook Pro 16-inch 2019 (CTBU).

--- cheatsheets/displayplacer.sh
+++ cheatsheets/displayplacer.sh
@@ -13,3 +13,6 @@ displayplacer "id:77E200AD-B0E6-0317-4BBF-F8B53DA3952C res:1440x900 color_depth:
 
 # JKI-MBA17 (without internal)
 displayplacer "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1024,80) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(3440,-560) degree:270"
+
+# MacBook Pro 16-inch 2019 (CTBU)
+displayplacer "id:9449BC59-B8F6-9D46-F60A-79D17EC0BFC0 res:1792x1120 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0" "id:BC61F0D3-E0F1-5E27-CA2C-5580455192FE res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1984,-1080) degree:0" "id:38799907-6835-3B7B-CAC9-E3C1FE7B1EFE res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-64,-1080) degree:0"
