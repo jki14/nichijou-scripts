6d739075c2258f529db0c55dc5c875377bdf0036
Jacky Wong <zwwn@amazon.com>
2025-04-11 08:18:25 +0900

    Update cheatsheet for displayplacer
    Make the profiles up-to-date.

--- cheatsheets/displayplacer.sh
+++ cheatsheets/displayplacer.sh
@@ -2,50 +2,50 @@
 
 # brew tap jakehilborn/jakehilborn && brew install displayplacer
 
-# MacBook Pro 16-inch 2019
-displayplacer "id:9449BC59-B8F6-9D46-F60A-79D17EC0BFC0 res:1792x1120 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(2616,-2000) degree:270" "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(-824,-1440) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1848,-1360) degree:90"
-
-# MacBook Pro 16-inch 2019 (without internal)
-displayplacer "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1024,80) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(3440,-560) degree:270"
-
 # JKI-MBA17
 displayplacer "id:77E200AD-B0E6-0317-4BBF-F8B53DA3952C res:1440x900 color_depth:4 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-2024,-1360) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(2440,-2000) degree:270" "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(-1000,-1440) degree:0"
 
 # JKI-MBA17 (without internal)
 displayplacer "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1024,80) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(3440,-560) degree:270"
 
+# MacBook Pro 16-inch 2019
+displayplacer "id:9449BC59-B8F6-9D46-F60A-79D17EC0BFC0 res:1792x1120 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(2616,-2000) degree:270" "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(-824,-1440) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1848,-1360) degree:90"
+
+# MacBook Pro 16-inch 2019 (without internal)
+displayplacer "id:03691118-6385-4E9B-6094-12A89CA7CE92 res:3440x1440 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:EC5694F1-0D4D-503E-66BA-C47F0B27397E res:1024x1280 hz:60 color_depth:8 scaling:off origin:(-1024,80) degree:90" "id:3DD93084-AE11-FAB8-8D67-497039F5EC4E res:1080x2560 hz:60 color_depth:8 scaling:off origin:(3440,-560) degree:270"
+
 # MacBook Pro 16-inch 2019 (CTBU)
 displayplacer "id:9449BC59-B8F6-9D46-F60A-79D17EC0BFC0 res:1792x1120 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0" "id:BC61F0D3-E0F1-5E27-CA2C-5580455192FE res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1984,-1080) degree:0" "id:38799907-6835-3B7B-CAC9-E3C1FE7B1EFE res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-64,-1080) degree:0"
-
-# JKI-MBP23 (34 + internal)
-displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0"
-
-# JKI-MBP23 (34 + 29 + internal)
-displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
-
-# JKI-MBP23 (34 + 29)
-displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
 
 # JKI-MBP23 (34 + 29 + HP-DOCK18 + internal)
 displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:1A072B43-945E-4B2D-A8B8-4FAD84721A54 res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
 
+# JKI-MBP23 (34 + 29 + internal)
+displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
+
+# JKI-MBP23 (34 + internal)
+displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0"
+
 # JKI-MBP23 (34 + 29 + HP-DOCK18)
 displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:1A072B43-945E-4B2D-A8B8-4FAD84721A54 res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
 
-# 7cf34dea6f6d (34 + internal)
-displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0"
+# JKI-MBP23 (34 + 29)
+displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
+
+# 7cf34dea6f6d (34 + 29 + HP-DOCK18 + internal)
+displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:F2CC5E2E-FCA6-4C6D-A193-742881162C1F res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
 
 # 7cf34dea6f6d (34 + 29 + internal)
 displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
 
+# 7cf34dea6f6d (34 + internal)
+displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0"
+
+# 7cf34dea6f6d (34 + 29 + HP-DOCK18)
+displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:F2CC5E2E-FCA6-4C6D-A193-742881162C1F res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
+
 # 7cf34dea6f6d (34 + 29)
 displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
-
-# 7cf34dea6f6d (34 + HP Dock 2018 + internal)
-displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:F2CC5E2E-FCA6-4C6D-A193-742881162C1F res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
-
-# 7cf34dea6f6d (34 + 29 + HP Dock 2018)
-displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:F2CC5E2E-FCA6-4C6D-A193-742881162C1F res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
 
 # 7cf34dea6f6d (U3415W)
 displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:EFC31249-16DC-4D76-9499-6D6C7719F9C8 res:3440x1440 hz:60 color_depth:8 enabled:true scaling:off origin:(-964,-1440) degree:0"
