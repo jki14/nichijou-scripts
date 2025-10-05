844b085d7841062c92c18b092a949cff4e8a547f
jki14 <jki14wz@gmail.com>
2025-10-06 06:46:46 +0800

    Update cheatsheet for displayplacer
    Add the first profile for LC34G55T.

--- cheatsheets/displayplacer.sh
+++ cheatsheets/displayplacer.sh
@@ -32,6 +32,9 @@ displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 colo
 # JKI-MBP23 (34 + 29)
 displayplacer "id:CC48629C-28F9-4D0D-A843-F7C81F026BF6 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:5A00D475-F03C-4036-AFA1-E51475986B59 res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270"
 
+# JKI-MBP23 (LC34G55T)
+displayplacer "id:4C579A76-006F-40B1-BC81-9E59AFC7F5E2 res:3440x1440 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0"
+
 # 7cf34dea6f6d (34 + 29 + HP-DOCK18 + internal)
 displayplacer "id:57AA4DF7-470A-4611-90A0-F4816BAD7921 res:3440x1440 hz:100 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(964,1440) degree:0" "id:AAF24370-EB8E-48FB-82DE-7E29995E7B7B res:1080x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3440,-560) degree:270" "id:F2CC5E2E-FCA6-4C6D-A193-742881162C1F res:1024x1280 hz:60 color_depth:4 enabled:true scaling:off origin:(-1024,80) degree:0"
 
