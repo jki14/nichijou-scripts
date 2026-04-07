289d7a84f2e0fac55871dc053dc596366926e2a8
jki14 <jki14wz@gmail.com>
2026-04-07 21:09:32 +0900

    Update NIX CheatSheets
    Add sample command for git commit with -12h timestamp.

--- cheatsheets/linux/nix.sh
+++ cheatsheets/linux/nix.sh
@@ -1,2 +1,5 @@
 # batch gz decompress
 for f in *.gz; do gunzip -c "$f" >"${f%.*}"; done
+
+# Git Commit at -12H
+DT="$(date -v-12H '+%Y-%m-%d %H:%M:%S %z')"; GIT_AUTHOR_DATE="${DT}" GIT_COMMITTER_DATE="${DT}" git commit
